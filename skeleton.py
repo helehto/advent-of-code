52cb8419a3d1e4662b9b9e960eb2d534cd48d4bc
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2022-11-29 15:07:23 +0100

    Initial commit

---
+++ skeleton.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import functools
+import itertools
+import numpy as np
+import re
+import sys
+from collections import Counter, defaultdict
+from dataclasses import dataclass, field
+
+
+def main():
+    lines = sys.stdin.readlines()
+
+
+if __name__ == '__main__':
+    main()
