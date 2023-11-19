e26e8420f6bb1ae2b3708d6e89644561181f649a
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2023-11-19 02:03:26 +0100

    2021/2: Solve
      Year    Day    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  -----------  --------  ----------  ----------
      2021      2      19.9907   1.08805      19.677      19.858

---
+++ 2021/2.cc
@@ -0,0 +1,33 @@
+#include "common.h"
+#include <fmt/core.h>
+#include <vector>
+
+int run_2021_2(FILE *f)
+{
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
+
+    std::string s;
+    while (getline(f, s)) {
+        int v = 0;
+
+        if (s.starts_with("up")) {
+            std::from_chars(s.data() + 3, s.data() + s.size(), v);
+            y1 -= v;
+        } else if (s.starts_with("down")) {
+            std::from_chars(s.data() + 5, s.data() + s.size(), v);
+            y1 += v;
+        } else { // forward
+            std::from_chars(s.data() + 8, s.data() + s.size(), v);
+            x1 += v;
+            x2 += v;
+            y2 += y1 * v;
+        }
+    }
+
+    fmt::print("{}\n", x1 * y1);
+    fmt::print("{}\n", x2 * y2);
+    return 0;
+}
