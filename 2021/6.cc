721ac08d0b4e4e04f99e65e7f604a1dd44a4a786
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2023-11-19 12:51:30 +0100

    2021/6: Solve
      Year    Day    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  -----------  --------  ----------  ----------
      2021      6       2.5371   0.15156       2.515       2.535

---
+++ 2021/6.cc
@@ -0,0 +1,25 @@
+#include "common.h"
+#include <fmt/core.h>
+#include <numeric>
+#include <span>
+#include <vector>
+
+int run_2021_6(FILE *f)
+{
+    std::array<uint64_t, 9> a = {{}};
+    std::string s;
+    getline(f, s);
+    for (auto i : find_numbers<uint8_t>(s))
+        a[i - 1]++;
+
+    int i = 0;
+    for (; i < 79; i++)
+        a[(i + 7) % a.size()] += a[i % a.size()];
+    fmt::print("{}\n", std::accumulate(begin(a), end(a), UINT64_C(0)));
+
+    for (; i < 255; i++)
+        a[(i + 7) % a.size()] += a[i % a.size()];
+    fmt::print("{}\n", std::accumulate(begin(a), end(a), UINT64_C(0)));
+
+    return 0;
+}
