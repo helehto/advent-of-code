4d0696cb5c686502a965f0edc4d0b5fa8ff09535
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2023-12-01 21:58:32 +0100

    2023/1: Speed up
    Instead of comparing individual characters in each position, which leads
    to O(m*n) behaviour, hold the next eight characters in the string in a
    64-bit integer as a rolling window. Masking and comparing this integer
    to an expected string packed in the same way is O(1) which brings the
    entire loop back down to O(n) complexity.
    
    (Also, the logic is now completely inlined which avoids the expensive
    call out to string_view::compare() and memcmp() for every character.)
    
    I really can't be bothered to implement Aho-Corasick for a few μs gain
    over this (at best).
    
      Year    Day    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  -----------  --------  ----------  ----------
      2023      1      94.2895   2.14888      93.557       94.91

--- 2023/1.cc
+++ 2023/1.cc
@@ -1,6 +1,14 @@
 #include "common.h"
 #include <fmt/core.h>
 
+constexpr uint64_t pack(std::string_view p)
+{
+    uint64_t r = 0;
+    for (size_t i = 0; i < p.size(); i++)
+        r |= static_cast<uint64_t>(p[i]) << (8 * i);
+    return r;
+}
+
 void run_2023_1(FILE *f)
 {
     int part1 = 0;
@@ -20,27 +28,36 @@ void run_2023_1(FILE *f)
 
         v.clear();
 
-        for (size_t i = 0; i < s.size(); i++) {
+        uint64_t window = 0;
+        for (size_t i = 0; i < std::min<size_t>(s.size(), 8); i++)
+            window |= static_cast<uint64_t>(s[i]) << (8 * i);
+
+        for (size_t i = 0; i < s.size();) {
             if (s[i] >= '0' && s[i] <= '9')
                 v.push_back(s[i] - '0');
-            else if (s.substr(i).starts_with("one"))
+            else if ((window & 0xffffff) == pack("one"))
                 v.push_back(1);
-            else if (s.substr(i).starts_with("two"))
+            else if ((window & 0xffffff) == pack("two"))
                 v.push_back(2);
-            else if (s.substr(i).starts_with("three"))
+            else if ((window & 0xffffffffff) == pack("three"))
                 v.push_back(3);
-            else if (s.substr(i).starts_with("four"))
+            else if ((window & 0xffffffff) == pack("four"))
                 v.push_back(4);
-            else if (s.substr(i).starts_with("five"))
+            else if ((window & 0xffffffff) == pack("five"))
                 v.push_back(5);
-            else if (s.substr(i).starts_with("six"))
+            else if ((window & 0xffffff) == pack("six"))
                 v.push_back(6);
-            else if (s.substr(i).starts_with("seven"))
+            else if ((window & 0xffffffffff) == pack("seven"))
                 v.push_back(7);
-            else if (s.substr(i).starts_with("eight"))
+            else if ((window & 0xffffffffff) == pack("eight"))
                 v.push_back(8);
-            else if (s.substr(i).starts_with("nine"))
+            else if ((window & 0xffffffff) == pack("nine"))
                 v.push_back(9);
+
+            i++;
+            window = window >> 8;
+            if (i + 7 < s.size())
+                window |= static_cast<uint64_t>(s[i + 7]) << 56;
         }
 
         part2 += v.front() * 10 + v.back();
