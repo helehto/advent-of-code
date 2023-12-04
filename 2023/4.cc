58feaf09815877628dc51e7a8d1db082a306271d
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2023-12-04 18:18:41 +0100

    2023/4: Solve
      Year    Day    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  -----------  --------  ----------  ----------
      2023      4      81.4157   2.75246      80.582      81.744

---
+++ 2023/4.cc
@@ -0,0 +1,39 @@
+#include "common.h"
+#include <fmt/core.h>
+#include <numeric>
+
+void run_2023_4(FILE *f)
+{
+    std::vector<int> matches;
+    std::string s;
+    std::vector<int> v;
+    while (getline(f, s)) {
+        find_numbers(s, v);
+
+        uint64_t winning[2] = {0};
+        for (size_t i = 1; i < 11; i++)
+            winning[v[i] / 64] |= UINT64_C(1) << (v[i] % 64);
+
+        uint64_t have[2] = {0};
+        for (size_t i = 11; i < v.size(); i++)
+            have[v[i] / 64] |= UINT64_C(1) << (v[i] % 64);
+
+        int n_correct = __builtin_popcountl(winning[0] & have[0]) +
+                        __builtin_popcountl(winning[1] & have[1]);
+        matches.push_back(n_correct);
+    }
+
+    int score_sum = 0;
+    for (int v : matches) {
+        if (v)
+            score_sum += 1 << (v - 1);
+    }
+    fmt::print("{}\n", score_sum);
+
+    std::vector<int> n_cards(matches.size(), 1);
+    for (size_t i = 0; i < n_cards.size(); i++) {
+        for (size_t j = 0; j < matches[i]; j++)
+            n_cards[i + j + 1] += n_cards[i];
+    }
+    fmt::print("{}\n", std::accumulate(n_cards.begin(), n_cards.end(), 0));
+}
