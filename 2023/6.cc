883643568f72381c4be7d4a637df15f032ed9c05
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2023-12-06 21:53:43 +0100

    2023/6: Solve
      Year    Day    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  -----------  --------  ----------  ----------
      2023      6     0.463953  0.174925        0.45       0.461

---
+++ 2023/6.cc
@@ -0,0 +1,29 @@
+#include "common.h"
+
+static uint64_t ways(uint64_t t, uint64_t d)
+{
+    return std::floor(t / 2. + sqrt(t * t / 4 - d) - 1e-6) -
+           std::ceil(t / 2. - sqrt(t * t / 4 - d) + 1e-6) + 1;
+}
+
+void run_2023_6(FILE *f)
+{
+    std::string s;
+    getline(f, s);
+    auto ts = find_numbers<int>(s);
+    getline(f, s);
+    auto ds = find_numbers<int>(s);
+
+    int prod = 1;
+    for (size_t i = 0; i < ts.size(); i++)
+        prod *= ways(ts[i], ds[i]);
+    fmt::print("{}\n", prod);
+
+    uint64_t t = 0;
+    uint64_t d = 0;
+    for (size_t i = 0; i < ts.size(); i++) {
+        t = pow(10, ceil(log10(ts[i]))) * t + ts[i];
+        d = pow(10, ceil(log10(ds[i]))) * d + ds[i];
+    }
+    fmt::print("{}\n", ways(t, d));
+}
