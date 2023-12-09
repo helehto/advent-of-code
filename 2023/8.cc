9b1c462f80280ad0df0f00e464c0d9fac54b70a9
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2023-12-09 22:27:36 +0100

    2023/8: Speed up
      Year    Day    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  -----------  --------  ----------  ----------
      2023      8      131.511   2.51809     129.745     133.162

--- 2023/8.cc
+++ 2023/8.cc
@@ -1,83 +1,44 @@
 #include "common.h"
-#include <fmt/ranges.h>
 #include <numeric>
-#include <unordered_map>
+
+constexpr uint16_t encode_name(std::string_view sv)
+{
+    return (sv[0] - 'A') << 10 | (sv[1] - 'A') << 5 | (sv[2] - 'A');
+}
 
 void run_2023_8(FILE *f)
 {
     std::string directions;
     getline(f, directions);
-    std::string unused;
-    getline(f, unused);
-    auto lines = getlines(f);
+    std::string s;
+    getline(f, s);
 
-    std::vector<std::pair<uint16_t, uint16_t>> next(lines.size());
-    std::unordered_map<std::string_view, uint16_t> name_to_index_map;
-
-    for (size_t i = 0; std::string_view s : lines) {
-        auto index_from_name = [&](std::string_view sv) {
-            auto [it, inserted] = name_to_index_map.emplace(sv, i);
-            return inserted ? i++ : it->second;
-        };
-
-        const size_t ai = index_from_name(s.substr(0, 3));
-        const size_t bi = index_from_name(s.substr(7, 3));
-        const size_t ci = index_from_name(s.substr(12, 3));
-        next[ai] = {bi, ci};
-    }
-
+    std::vector<std::array<uint16_t, 2>> next(1 << 15);
     std::vector<size_t> starts;
-    size_t aaa = 0;
-    size_t zzz = 0;
-    for (const auto &[k, i] : name_to_index_map) {
-        if (k == "AAA")
-            aaa = i;
-        if (k == "ZZZ")
-            zzz = i;
-        if (k.ends_with("A"))
-            starts.push_back(i);
+    while (getline(f, s)) {
+        std::string_view sv = s;
+        const uint16_t ai = encode_name(sv.substr(0, 3));
+        const uint16_t bi = encode_name(sv.substr(7, 3));
+        const uint16_t ci = encode_name(sv.substr(12, 3));
+        next[ai] = {{bi, ci}};
+        if ((ai & 0b11111) == 0)
+            starts.push_back(ai);
     }
 
-    struct State8 {
-        size_t curr;
-        size_t dir_index = 0;
-
-        bool operator==(const State8 &) const = default;
+    auto walk = [&](size_t start) {
+        size_t n = 0;
+        for (size_t i = 0, k = start; (k & 0b11111) != 25; i++, n++) {
+            if (i >= directions.size())
+                i = 0;
+            k = next[k][directions[i] == 'R'];
+        }
+        return n;
     };
 
-    auto step = [&](State8 s) {
-        State8 new_state;
-        new_state.curr =
-            directions[s.dir_index] == 'L' ? next[s.curr].first : next[s.curr].second;
-        new_state.dir_index = s.dir_index + 1;
-        if (new_state.dir_index >= directions.size())
-            new_state.dir_index -= directions.size();
-        return new_state;
-    };
-
-    auto s1 = step({aaa});
-    size_t steps = 1;
-    for (; s1.curr != zzz; steps++)
-        s1 = step(s1);
-    fmt::print("{}\n", steps);
+    fmt::print("{}\n", walk(encode_name("AAA")));
 
     uint64_t part2 = 1;
-    for (size_t k : starts) {
-        int cycle_length = 1;
-        State8 slow{k};
-        State8 fast = step(slow);
-
-        // Brent's cycle detection algorithm
-        for (int power = 1; slow != fast; cycle_length++) {
-            if (power == cycle_length) {
-                slow = fast;
-                power *= 2;
-                cycle_length = 0;
-            }
-            fast = step(fast);
-        }
-
-        part2 = std::lcm(part2, cycle_length);
-    }
+    for (size_t k : starts)
+        part2 = std::lcm(part2, walk(k));
     fmt::print("{}\n", part2);
 }
