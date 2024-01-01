3a4e749f19f42068543866dc2de22e8253f9d874
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-01-01 13:27:01 +0100

    2023/17: Optimize
    Most of the speed-up here comes from replacing Boost's fibonacci_heap
    with a custom d-ary heap.
    
      Year    Day    n0    n1    min t₀    min t₁      Δmin  Δmin%      avg t₀    avg t₁      Δavg  Δavg%
    ------  -----  ----  ----  --------  --------  --------  -------  --------  --------  --------  -------
      2023     17   142   606     38012   8824.27  -29187.7  -76.8%    39199.7   8940.47  -30259.2  -77.2%

--- 2023/17.cc
+++ 2023/17.cc
@@ -1,104 +1,106 @@
 #include "common.h"
-#include "dense_map.h"
-#include <boost/heap/fibonacci_heap.hpp>
 
 namespace aoc_2023_17 {
 
 enum { N, E, S, W };
 
-struct Vertex {
+struct alignas(4) Vertex {
     Point<uint8_t> p;
     uint8_t direction;
+    uint8_t padding;
 
-    bool operator==(const Vertex &v) const = default;
+    uint32_t to_u32() const { return std::bit_cast<uint32_t>(*this); }
+    static Vertex from_u32(uint32_t x) { return std::bit_cast<Vertex>(x); }
 };
+static_assert(sizeof(Vertex) == 4);
 
-} // namespace aoc_2023_17
-
-template <>
-struct std::hash<aoc_2023_17::Vertex> {
-    constexpr size_t operator()(const aoc_2023_17::Vertex &v) const
-    {
-        size_t h = 0;
-        hash_combine(h, v.p, v.direction);
-        return h;
-    }
-};
-
-namespace aoc_2023_17 {
-
-static Point<uint8_t> step(Point<uint8_t> p, uint8_t d, int n)
+static Point<uint8_t> step(Point<uint8_t> p, uint8_t d)
 {
-    static int8_t table[] = {0, 1, 0, -1};
-    return p.translate(n * table[d], n * table[(d - 1) & 3]);
+    int dx = d == W ? -1 : d == E ? 1 : 0;
+    int dy = d == N ? -1 : d == S ? 1 : 0;
+    return p.translate(dx, dy);
 }
 
 struct Neighbor {
-    Vertex v;
+    uint32_t v;
     int cost;
 };
 
-static boost::container::static_vector<Neighbor, 32>
-get_neighbors(const Matrix<uint8_t> &m, Vertex u, int min, int max)
+static std::span<Neighbor> get_neighbors(std::array<Neighbor, 16> &result,
+                                         const Matrix<uint8_t> &m,
+                                         uint32_t node,
+                                         int min,
+                                         int max)
 {
-    boost::container::static_vector<Neighbor, 32> result;
+    size_t count = 0;
+    auto u = Vertex::from_u32(node);
 
-    for (int turn : {-1, 1}) {
+    for (int turn = -1; turn <= 1; turn += 2) {
         uint8_t new_direction = (u.direction + turn) & 3;
-
+        Point<uint8_t> p = u.p;
         int cost = 0;
-        for (int n = 1; n <= max; n++) {
-            auto next = step(u.p, new_direction, n);
-            if (!m.in_bounds(next))
+
+        int n = 1;
+        for (n = 1; n < min; n++) {
+            p = step(p, new_direction);
+            if (!m.in_bounds(p))
                 break;
-            cost += m(next);
-            if (n >= min)
-                result.push_back({{next, new_direction}, cost});
+            cost += m(p);
+        }
+
+        for (; n <= max; n++) {
+            p = step(p, new_direction);
+            if (!m.in_bounds(p))
+                break;
+            cost += m(p);
+            result[count++] = {Vertex{p, new_direction}.to_u32(), cost};
         }
     }
 
-    return result;
+    return {result.data(), result.data() + count};
 }
 
-static int dijkstra(const Matrix<uint8_t> &m,
-                    dense_map<Vertex, int> &dist,
-                    int min_straight,
-                    int max_straight,
+static int dijkstra(const Matrix<uint8_t> &grid,
+                    std::initializer_list<uint32_t> start,
+                    std::vector<int> &dist,
+                    const int min_straight,
+                    const int max_straight,
                     Point<uint8_t> goal)
 {
-    auto cmp = [](auto &a, auto &b) { return a.second > b.second; };
-    using heap_type = boost::heap::fibonacci_heap<std::pair<Vertex, int>,
-                                                  boost::heap::compare<decltype(cmp)>>;
-    heap_type heap(cmp);
-    dense_map<Vertex, heap_type::handle_type> handles;
+    using VertexPlusDist = std::pair<uint32_t, int>;
 
-    for (auto &u : {Vertex{{0, 0}, S}, Vertex{{0, 0}, E}}) {
-        handles.emplace(u, heap.emplace(u, 0));
+    auto proj = [&](const VertexPlusDist &v) { return v.second; };
+    DHeap<VertexPlusDist, decltype(proj)> heap(proj);
+
+    std::vector<decltype(heap)::Handle> handles(1 << 20);
+
+    for (uint32_t u : start) {
         dist[u] = 0;
+        handles[u] = heap.push({u, 0});
     }
 
     while (!heap.empty()) {
         auto [u, u_dist] = heap.top();
         heap.pop();
-        handles.erase(u);
 
-        if (u.p == goal)
+        if (Vertex::from_u32(u).p == goal)
             return u_dist;
 
-        for (const auto &[v, cost] : get_neighbors(m, u, min_straight, max_straight)) {
+        std::array<Neighbor, 16> n;
+        for (auto [v, cost] : get_neighbors(n, grid, u, min_straight, max_straight)) {
             const auto new_dist = u_dist + cost;
 
-            auto [it, inserted] = dist.emplace(v, new_dist);
-            if (!inserted && it->second <= new_dist)
+            if (new_dist >= dist[v])
                 continue;
-            it->second = new_dist;
 
-            if (auto [it, inserted] = handles.try_emplace(v); !inserted) {
-                (*it->second).second = new_dist;
-                heap.increase(it->second);
-            } else {
-                it->second = heap.emplace(v, new_dist);
+            const auto old_dist = std::exchange(dist[v], new_dist);
+            if (old_dist == INT_MAX) {
+                handles[v] = heap.push({v, new_dist});
+                continue;
             }
+
+            heap.get(handles[v]).second = new_dist;
+            heap.decrease_key(handles[v]);
         }
     }
 
@@ -114,10 +116,14 @@ void run(FILE *f)
         static_cast<uint8_t>(grid.rows - 1),
     };
 
-    dense_map<Vertex, int> dist;
-    fmt::print("{}\n", dijkstra(grid, dist, 0, 3, goal));
-    dist.clear();
-    fmt::print("{}\n", dijkstra(grid, dist, 4, 10, goal));
+    std::initializer_list<uint32_t> start = {
+        Vertex{{0, 0}, S}.to_u32(),
+        Vertex{{0, 0}, E}.to_u32(),
+    };
+    std::vector<int> dist(1 << 20, INT_MAX);
+    fmt::print("{}\n", dijkstra(grid, start, dist, 0, 3, goal));
+    std::ranges::fill(dist, INT_MAX);
+    fmt::print("{}\n", dijkstra(grid, start, dist, 4, 10, goal));
 }
 
 }
