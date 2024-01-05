29116a26fb6af295a88989074796dd2e6629dd93
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-01-05 10:31:54 +0100

    2018/5: Solve
      Year    Day    Iterations    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  --------  ----------  ----------
      2018      5          1107       4519.8   113.768     4485.43     4536.85

---
+++ 2018/5.cc
@@ -0,0 +1,40 @@
+#include "common.h"
+
+namespace aoc_2018_5 {
+
+static size_t react(std::string &polymer, std::string_view input)
+{
+    polymer.clear();
+
+    for (char c : input) {
+        if (!polymer.empty() && (polymer.back() ^ 0x20) == c)
+            polymer.pop_back();
+        else
+            polymer.push_back(c);
+    }
+
+    return polymer.size();
+}
+
+void run(FILE *f)
+{
+    auto [_, lines] = slurp_lines(f);
+    auto s = lines[0];
+
+    std::string result;
+    fmt::print("{}\n", react(result, {s}));
+
+    size_t min = SIZE_MAX;
+    std::string spliced;
+    spliced.reserve(s.size());
+    for (char c = 'a'; c <= 'z'; c++) {
+        spliced.clear();
+        for (char k : lines[0])
+            if ((k | 0x20) != c)
+                spliced.push_back(k);
+        min = std::min(min, react(result, spliced));
+    }
+    fmt::print("{}\n", min);
+}
+
+}
