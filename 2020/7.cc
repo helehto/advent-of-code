aef4107095a74e9d8b13d26e86687af931f78f8c
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-12-24 17:26:05 +0100

    2020/7: Solve in C++
      Year    Day    Iterations    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  --------  ----------  ----------
      2020      7         33181      133.336   6.54145     125.127     141.217

---
+++ 2020/7.cc
@@ -0,0 +1,67 @@
+#include "common.h"
+#include "dense_map.h"
+#include "dense_set.h"
+
+namespace aoc_2020_7 {
+
+void run(FILE *f)
+{
+    auto [buf, lines] = slurp_lines(f);
+
+    std::vector<std::string_view> bags;
+    using Edge = std::pair<std::string_view, int>;
+    dense_map<std::string_view, std::vector<Edge>> predecessors;
+    dense_map<std::string_view, std::vector<Edge>> successors;
+    for (std::string_view line : lines) {
+        const size_t i = line.find(" bags contain ");
+        ASSERT(i != std::string_view::npos);
+        split(line.substr(i + 14), bags, ',');
+        for (std::string_view s : bags) {
+            int weight = -1;
+            s = strip(s);
+            auto r = std::from_chars(s.data(), s.data() + s.size(), weight);
+            if (r.ec != std::errc())
+                break;
+            std::string_view bag2(r.ptr, s.data() + s.find(" bag") - r.ptr);
+            predecessors[strip(bag2)].emplace_back(strip(line.substr(0, i)), weight);
+            successors[strip(line.substr(0, i))].emplace_back(strip(bag2), weight);
+        }
+    }
+
+    // Part 1:
+    {
+        std::vector<std::string_view> queue{"shiny gold"};
+        dense_set<std::string_view> visited;
+        while (!queue.empty()) {
+            std::string_view v = queue.back();
+            queue.pop_back();
+            visited.insert(v);
+
+            if (auto it = predecessors.find(v); it != end(predecessors)) {
+                for (auto &[u, _] : it->second)
+                    queue.push_back(u);
+            }
+        }
+        fmt::print("{}\n", visited.size() - 1);
+    }
+
+    // Part 2:
+    {
+        std::vector<std::pair<std::string_view, int>> queue{{"shiny gold", 1}};
+        int sum_weight = 0;
+        while (!queue.empty()) {
+            auto [u, count] = queue.back();
+            queue.pop_back();
+            sum_weight += count;
+
+            if (auto it = successors.find(u); it != end(successors)) {
+                for (auto &[v, weight] : it->second) {
+                    queue.push_back({v, count * weight});
+                }
+            }
+        }
+        fmt::print("{}\n", sum_weight - 1);
+    }
+}
+
+}
