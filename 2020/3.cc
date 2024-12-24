375677bc995596ef80b6b07cdf18e77112c57d65
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-12-24 13:38:46 +0100

    2020/3: Solve in C++
      Year    Day    Iterations    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  --------  ----------  ----------
      2020      3        908865      4.91424  0.129993       4.639       4.999

---
+++ 2020/3.cc
@@ -0,0 +1,29 @@
+#include "common.h"
+
+namespace aoc_2020_3 {
+
+void run(FILE *f)
+{
+    auto [buf, lines] = slurp_lines(f);
+    auto grid = Matrix<char>::from_lines(lines);
+
+    int slope_trees[5]{};
+    constexpr std::pair<int, int> walks[] = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
+    for (size_t i = 0; const auto &[dx, dy] : walks) {
+        Point<size_t> p(0, 0);
+        while (true) {
+            p = p.translate(dx, dy);
+            if (p.y >= grid.rows)
+                break;
+            if (p.x >= grid.cols)
+                p.x -= grid.cols;
+            slope_trees[i] += grid(p) == '#';
+        }
+        i++;
+    }
+    fmt::print("{}\n", slope_trees[1]);
+    fmt::print("{}\n", static_cast<uint64_t>(slope_trees[0]) * slope_trees[1] *
+                           slope_trees[2] * slope_trees[3] * slope_trees[4]);
+}
+
+}
