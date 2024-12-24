608f40e16332b7480668877c4e17e6e75ad9bc6f
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-12-24 14:58:36 +0100

    2020/5: Solve in C++
      Year    Day    Iterations    Mean (μs)     σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  ---------  ----------  ----------
      2020      5       1012546      4.40539  0.0403391       4.368       4.448

---
+++ 2020/5.cc
@@ -0,0 +1,33 @@
+#include "common.h"
+
+namespace aoc_2020_5 {
+
+constexpr static auto char2bit = [] {
+    std::array<uint8_t, 256> bit;
+    bit.fill(1);
+    bit['F'] = 0;
+    bit['L'] = 0;
+    return bit;
+}();
+
+void run(FILE *f)
+{
+    auto [buf, lines] = slurp_lines(f);
+
+    uint64_t id_sum = 0;
+    int min_id = INT_MAX;
+    int max_id = 0;
+    for (std::string_view line : lines) {
+        int id = 0;
+        for (const uint8_t c : line)
+            id = (id << 1) | char2bit[c];
+        min_id = std::min(min_id, id);
+        max_id = std::max(max_id, id);
+        id_sum += id;
+    }
+
+    fmt::print("{}\n", max_id);
+    fmt::print("{}\n", max_id * (max_id + 1) / 2 - min_id * (min_id - 1) / 2 - id_sum);
+}
+
+}
