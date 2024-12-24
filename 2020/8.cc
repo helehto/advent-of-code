c6d986ec79ca8bd1dda0cd8f0412ad63f436a18f
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-12-24 21:08:31 +0100

    2020/8: Solve in C++
    A brute-force approach, but it runs quickly enough. Being more clever
    about this by actually analyzing the program would probably make this
    run in under 10 μs.
    
      Year    Day    Iterations    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  --------  ----------  ----------
      2020      8         37887      118.395  0.335083     117.984     118.855

---
+++ 2020/8.cc
@@ -0,0 +1,73 @@
+#include "common.h"
+
+namespace aoc_2020_8 {
+
+enum { NOP, JMP, ACC };
+struct Instruction {
+    uint16_t op : 2;
+    int16_t val : 14;
+};
+
+std::pair<int, bool> run(const std::vector<Instruction> &prog)
+{
+    std::vector<bool> executed(prog.size(), false);
+    int acc = 0;
+    size_t pc = 0;
+    while (pc < prog.size() && !executed[pc]) {
+        executed[pc] = true;
+        int op = prog[pc].op;
+        int val = prog[pc].val;
+        if (op == ACC) {
+            acc += val;
+            pc++;
+        }
+        if (op == JMP) {
+            pc += val;
+        }
+        if (op == NOP) {
+            pc++;
+        }
+    }
+
+    return {acc, pc >= prog.size()};
+}
+
+void run(FILE *f)
+{
+    auto [buf, lines] = slurp_lines(f);
+
+    std::vector<Instruction> prog;
+    std::vector<int> nums;
+    prog.reserve(size(lines));
+    for (std::string_view line : lines) {
+        Instruction &instr = prog.emplace_back();
+        std::string_view mnemonic = line.substr(0, 3);
+        if (mnemonic == "nop")
+            instr.op = NOP;
+        else if (mnemonic == "acc")
+            instr.op = ACC;
+        else if (mnemonic == "jmp")
+            instr.op = JMP;
+        else
+            ASSERT(false);
+
+        find_numbers(line, nums);
+        ASSERT(nums.size() == 1);
+        instr.val = nums[0];
+    }
+
+    // Part 1:
+    fmt::print("{}\n", run(prog).first);
+
+    // Part 2:
+    for (size_t i = 0; i < prog.size(); i++) {
+        prog[i].op ^= 1;
+        if (auto [acc, terminated] = run(prog); terminated) {
+            fmt::print("{}\n", acc);
+            break;
+        }
+        prog[i].op ^= 1;
+    }
+}
+
+}
