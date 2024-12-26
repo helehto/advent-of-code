d4bf00cbb0f38806c0e11ffa84db55f84409e6d7
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-12-26 02:08:38 +0100

    2020/12: Solve in C++
      Year    Day    Iterations    Mean (μs)     σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  ---------  ----------  ----------
      2020     12        740438      6.02629  0.0550853       5.981       6.092

---
+++ 2020/12.cc
@@ -0,0 +1,76 @@
+#include "common.h"
+
+namespace aoc_2020_12 {
+
+void run(FILE *f)
+{
+    const auto [buf, lines] = slurp_lines(f);
+
+    std::vector<int> nums;
+    nums.reserve(lines.size());
+    find_numbers(buf, nums);
+
+    Point<int> p(0, 0);
+    int dx = 1;
+    int dy = 0;
+    for (size_t i = 0; std::string_view line : lines) {
+        const char c = line.front();
+        const int value = nums[i];
+        if (c == 'N') {
+            p.y -= value;
+        } else if (c == 'S') {
+            p.y += value;
+        } else if (c == 'E') {
+            p.x += value;
+        } else if (c == 'W') {
+            p.x -= value;
+        } else if (c == 'R') {
+            int turns = value / 90;
+            for (int i = 0; i < turns; ++i) {
+                std::tie(dx, dy) = std::pair(-dy, dx);
+            }
+        } else if (c == 'L') {
+            int turns = value / 90;
+            for (int i = 0; i < turns; ++i) {
+                std::tie(dx, dy) = std::pair(dy, -dx);
+            }
+        } else if (c == 'F') {
+            p = p.translate(value * dx, value * dy);
+        }
+        i++;
+    }
+    fmt::print("{}\n", manhattan<int>(p, {0, 0}));
+
+    std::tie(dx, dy) = std::pair(1, 0);
+    p = {0, 0};
+    Point<int> waypoint{10, -1};
+    for (size_t i = 0; std::string_view line : lines) {
+        const char c = line.front();
+        const int value = nums[i];
+        if (c == 'N') {
+            waypoint.y -= value;
+        } else if (c == 'S') {
+            waypoint.y += value;
+        } else if (c == 'E') {
+            waypoint.x += value;
+        } else if (c == 'W') {
+            waypoint.x -= value;
+        } else if (c == 'R') {
+            int turns = value / 90;
+            for (int i = 0; i < turns; ++i) {
+                std::tie(waypoint.x, waypoint.y) = std::pair(-waypoint.y, waypoint.x);
+            }
+        } else if (c == 'L') {
+            int turns = value / 90;
+            for (int i = 0; i < turns; ++i) {
+                std::tie(waypoint.x, waypoint.y) = std::pair(waypoint.y, -waypoint.x);
+            }
+        } else if (c == 'F') {
+            p = p.translate(value * waypoint.x, value * waypoint.y);
+        }
+        i++;
+    }
+    fmt::print("{}\n", manhattan<int>(p, {0, 0}));
+}
+
+}
