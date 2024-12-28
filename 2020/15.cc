2b14981a38f733d623955fd3fd0e731f2f0cd2e6
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2024-12-28 02:49:16 +0100

    2020/15: Solve in C++
      Year    Day    Iterations    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  --------  ----------  ----------
      2020     15            24       193287   3357.58      189900      196858

---
+++ 2020/15.cc
@@ -0,0 +1,31 @@
+#include "common.h"
+
+namespace aoc_2020_15 {
+
+static int32_t step(int32_t turn, int32_t last, std::vector<int32_t> &seen)
+{
+    const int32_t t = std::exchange(seen[last], turn - 1);
+    return t >= 0 ? turn - t - 1 : 0;
+}
+
+void run(FILE *f)
+{
+    auto [buf, _] = slurp_lines(f);
+    auto init = find_numbers<int32_t>(buf);
+
+    std::vector<int32_t> seen(30'000'000, -1);
+    for (size_t i = 0; i < init.size() - 1; i++)
+        seen[init[i]] = i + 1;
+
+    int32_t last = init.back();
+    int32_t turn = init.size() + 1;
+    for (; turn <= 2020; turn++)
+        last = step(turn, last, seen);
+    fmt::print("{}\n", last);
+
+    for (; turn <= 30'000'000; turn++)
+        last = step(turn, last, seen);
+    fmt::print("{}\n", last);
+}
+
+}
