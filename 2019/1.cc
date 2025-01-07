616ddb433d0d274c8e6bed3d1f44735c02add0b3
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-01-07 04:46:43 +0100

    2019/1: Solve in C++
      Year    Day    Iterations    Mean (μs)     σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  ---------  ----------  ----------
      2019      1       3014943      1.48044  0.0103886       1.463       1.493

---
+++ 2019/1.cc
@@ -0,0 +1,28 @@
+#include "common.h"
+
+namespace aoc_2019_1 {
+
+void run(FILE *f)
+{
+    auto [buf, _] = slurp_lines(f);
+    auto nums = find_numbers<int>(buf);
+
+    int sum = 0;
+    for (auto n : nums)
+        sum += n / 3 - 2;
+
+    int sum2 = 0;
+    for (auto n : nums) {
+        int s = 0;
+        while (true) {
+            n = n / 3 - 2;
+            if (n <= 0)
+                break;
+            s += n;
+        }
+        sum2 += s;
+    }
+    fmt::print("{}\n{}\n", sum, sum2);
+}
+
+}
