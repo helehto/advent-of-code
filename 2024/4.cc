cf883f77045e6c723ca91a51ab82ae5158bb1558
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-02-06 21:50:46 +0100

    2024/4: Solve in C++

---
+++ 2024/4.cc
@@ -0,0 +1,66 @@
+#include "common.h"
+
+namespace aoc_2024_4 {
+
+constexpr bool
+scan(const Matrix<char> &grid, Point<int> start, int dx, int dy, int len, char *out)
+{
+    for (int n = 0; n < len; ++n) {
+        auto p = start.translate(n * dx, n * dy);
+        if (!grid.in_bounds(p))
+            return false;
+        *out++ = grid(p);
+    }
+    return true;
+}
+
+constexpr bool
+match_word(const std::array<char, 4> &word, char c1, char c2, char c3, char c4)
+{
+    // Compare words using a single 32-bit comparison.
+    return std::bit_cast<uint32_t>(word) ==
+           std::bit_cast<uint32_t>(std::array<char, 4>{c1, c2, c3, c4});
+}
+
+constexpr int part1(const Matrix<char> &grid)
+{
+    alignas(4) std::array<char, 4> word;
+    int result = 0;
+
+    for (auto p : grid.ndindex<int>())
+        for (int dx : {-1, 0, 1})
+            for (int dy : {-1, 0, 1})
+                result += scan(grid, p, dx, dy, 4, word.data()) &&
+                          match_word(word, 'X', 'M', 'A', 'S');
+
+    return result;
+}
+
+constexpr int part2(const Matrix<char> &grid)
+{
+    alignas(4) std::array<char, 4> word;
+    int result = 0;
+
+    for (auto p : grid.ndindex<int>()) {
+        auto check = [&](Point<int> start, int dx, int dy) {
+            word.fill(0);
+            return scan(grid, start, dx, dy, 3, word.data()) &&
+                   (match_word(word, 'M', 'A', 'S', 0) ||
+                    match_word(word, 'S', 'A', 'M', 0));
+        };
+
+        result += check(p, 1, 1) && check(p.translate(2, 0), -1, 1);
+    }
+
+    return result;
+}
+
+void run(FILE *f)
+{
+    auto [_, lines] = slurp_lines(f);
+    auto grid = Matrix<char>::from_lines(lines);
+    fmt::print("{}\n", part1(grid));
+    fmt::print("{}\n", part2(grid));
+}
+
+}
