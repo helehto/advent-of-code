316155a940c4d667f2e39a30a7ec0953680f3d61
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-02-06 20:43:17 +0100

    2024/1: Solve in C++

---
+++ 2024/1.cc
@@ -0,0 +1,49 @@
+#include "common.h"
+#include "dense_map.h"
+
+namespace aoc_2024_1 {
+
+static int part1(std::span<const int> l, std::span<const int> r)
+{
+    int result = 0;
+    for (size_t i = 0; i < l.size(); ++i)
+        result += std::abs(l[i] - r[i]);
+    return result;
+}
+
+static int part2(std::span<const int> l, std::span<const int> r)
+{
+    int result = 0;
+    dense_map<int, int> freqs;
+    freqs.reserve(l.size());
+    for (int b : r)
+        freqs[b]++;
+    for (int a : l) {
+        auto it = freqs.find(a);
+        result += a * (it != freqs.end() ? it->second : 0);
+    }
+    return result;
+}
+
+void run(FILE *f)
+{
+    auto [buf, lines] = slurp_lines(f);
+
+    std::vector<int> nums;
+    std::vector<int> l, r;
+    l.reserve(lines.size());
+    r.reserve(lines.size());
+    for (std::string_view line : lines) {
+        find_numbers(line, nums);
+        l.push_back(nums[0]);
+        r.push_back(nums[1]);
+    }
+
+    std::ranges::sort(l);
+    std::ranges::sort(r);
+
+    fmt::print("{}\n", part1(l, r));
+    fmt::print("{}\n", part2(l, r));
+}
+
+}
