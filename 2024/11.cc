a490615b15daf73334f95a73aa6b82c005aab03f
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-02-09 01:18:46 +0100

    2024/11: Solve in C++

---
+++ 2024/11.cc
@@ -0,0 +1,47 @@
+#include "common.h"
+#include "dense_map.h"
+
+namespace aoc_2024_11 {
+
+static void step(dense_map<int64_t, int64_t> &counter,
+                 dense_map<int64_t, int64_t> &result)
+{
+    result.clear();
+
+    for (auto [num, count] : counter) {
+        if (num == 0) {
+            result[1] += count;
+        } else if (const auto d = digit_count_base10(num); d % 2 == 0) {
+            auto [a, b] = std::div(num, pow10i[digit_count_base10(num) / 2]);
+            result[a] += count;
+            result[b] += count;
+        } else {
+            result[num * 2024] += count;
+        }
+    }
+}
+
+void run(FILE *f)
+{
+    auto buf = slurp(f);
+    std::vector<int64_t> nums;
+    find_numbers(buf, nums);
+
+    dense_map<int64_t, int64_t> counter;
+    dense_map<int64_t, int64_t> tmp;
+    for (auto n : nums)
+        counter[n]++;
+
+    for (int steps : {25, 50}) {
+        for (int64_t i = 0; i < steps; i++) {
+            step(counter, tmp);
+            counter.swap(tmp);
+        }
+        int64_t total = 0;
+        for (auto &[_, freq] : counter)
+            total += freq;
+        fmt::print("{}\n", total);
+    }
+}
+
+}
