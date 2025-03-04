ad2418d8f6143c84a15f80715b1baa1c93c4896d
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-03-04 18:27:30 +0100

    2024/22: Optimize

--- 2024/22.cc
+++ 2024/22.cc
@@ -1,63 +1,62 @@
 #include "common.h"
-#include "dense_map.h"
-#include "dense_set.h"
+#include "inplace_vector.h"
+#include <bitset>
 
 namespace aoc_2024_22 {
 
 void run(std::string_view buf)
 {
+    auto lines = split_lines(buf);
     std::vector<int> nums;
+    constexpr int N = 2000;
 
-    std::vector<std::vector<int>> secrets;
+    std::vector<inplace_vector<int, N + 1>> secrets;
+    secrets.reserve(lines.size());
     for (std::string_view line : split_lines(buf)) {
         auto [n] = find_numbers_n<int, 1>(line);
-        std::vector<int> s;
-        s.reserve(2002);
-        s.push_back(n);
+        auto &s = secrets.emplace_back();
+        s.unchecked_push_back(n);
 
-        while (s.size() <= 2001) {
+        while (s.size() <= N) {
             auto n = s.back();
             n = ((n << 6) ^ n) & 16777215;
             n = ((n >> 5) ^ n) & 16777215;
             n = ((n << 11) ^ n) & 16777215;
-            s.push_back(n);
+            s.unchecked_push_back(n);
         }
-
-        secrets.push_back(std::move(s));
     }
 
     int64_t s1 = 0;
     for (auto &s : secrets)
-        s1 += s[2000];
+        s1 += s[N];
     fmt::print("{}\n", s1);
 
-    std::vector<std::vector<int>> prices;
-
+    std::vector<inplace_vector<int, N>> prices;
+    prices.reserve(lines.size());
     for (auto &s : secrets) {
-        std::vector<int> p;
-        p.reserve(s.size() - 1);
+        auto &p = prices.emplace_back();
         for (size_t i = 1; i < s.size(); i++)
-            p.push_back(s[i] % 10 - s[i - 1] % 10);
-        prices.push_back(p);
+            p.unchecked_push_back(s[i] % 10 - s[i - 1] % 10 + 9);
     }
 
-    dense_map<std::tuple<int, int, int, int>, std::vector<int>, TuplelikeHasher>
-        sequences;
-    dense_set<std::tuple<int, int, int, int>, TuplelikeHasher> seen;
+    std::vector<std::vector<int>> sequences(19 * 19 * 19 * 19);
+    std::bitset<19 * 19 * 19 * 19> seen;
     for (size_t i = 0; i < secrets.size(); i++) {
-        seen.clear();
+        seen.reset();
         const auto &s = secrets[i];
         const auto &p = prices[i];
 
         for (size_t j = 4; j < p.size(); ++j) {
-            std::tuple seq(p[j - 4], p[j - 3], p[j - 2], p[j - 1]);
-            if (auto [_, inserted] = seen.insert(seq); inserted)
-                sequences[seq].push_back(s[j] % 10);
+            uint32_t key = 19 * (19 * (19 * p[j - 4] + p[j - 3]) + p[j - 2]) + p[j - 1];
+            if (!seen.test(key)) {
+                seen.set(key);
+                sequences[key].push_back(s[j] % 10);
+            }
         }
     }
 
     std::vector<int64_t> q;
-    for (auto &[_, seq] : sequences) {
+    for (auto &seq : sequences) {
         int64_t t = 0;
         for (auto v : seq)
             t += v;
