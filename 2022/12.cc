015383542d33a78798c3d896587d9eee8a5ff3f0
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-05-19 20:55:49 +0200

    2022/12: Optimize and replace binary heap with a bucket queue
    Using a bucket queue is much simpler and faster.

--- 2022/12.cc
+++ 2022/12.cc
@@ -1,43 +1,38 @@
 #include "common.h"
-#include <algorithm>
-#include <climits>
+#include "monotonic_bucket_queue.h"
 
 namespace aoc_2022_12 {
 
-static int dijkstra(const Matrix<char> &m, size_t target, std::vector<int> &dist)
+static int dijkstra(const Matrix<char> &m, size_t target, std::vector<uint32_t> &dist)
 {
-    BinaryHeap q(λab(dist[a] < dist[b]), m.size());
-    std::vector<size_t> neighbors;
+    MonotonicBucketQueue<uint32_t> bq(2);
 
-    neighbors.reserve(4);
+    for (size_t i = 0; i < m.size(); ++i)
+        if (m.data[i] == 'a')
+            bq.emplace(0, i);
 
-    while (!q.empty()) {
-        auto u = q.top();
-        q.pop();
+    while (std::optional<uint32_t> u = bq.pop()) {
+        if (dist[*u] != bq.current_priority())
+            continue;
 
-        const size_t x = u % m.cols;
-        const size_t y = u / m.cols;
-
-        neighbors.clear();
-        if (x > 0)
-            neighbors.emplace_back(u - 1);
-        if (x + 1 < m.cols)
-            neighbors.emplace_back(u + 1);
-        if (y > 0)
-            neighbors.emplace_back(u - m.cols);
-        if (y + 1 < m.rows)
-            neighbors.emplace_back(u + m.cols);
-
-        for (size_t v : neighbors) {
-            if (m.data[v] > m.data[u] + 1)
-                continue;
-
-            auto new_dist = dist[u] + 1;
-            if (new_dist < dist[v]) {
-                dist[v] = new_dist;
-                q.decrease(v);
+        auto expand = [&](size_t v) {
+            if (m.data[v] <= m.data[*u] + 1) {
+                if (auto new_dist = dist[*u] + 1; new_dist < dist[v]) {
+                    dist[v] = new_dist;
+                    bq.emplace(new_dist, v);
+                }
             }
-        }
+        };
+
+        const size_t x = *u % m.cols;
+        if (x > 0)
+            expand(*u - 1);
+        if (x + 1 < m.cols)
+            expand(*u + 1);
+        if (*u >= m.cols)
+            expand(*u - m.cols);
+        if (*u + m.cols < m.size())
+            expand(*u + m.cols);
     }
 
     return dist[target];
@@ -45,31 +40,24 @@ static int dijkstra(const Matrix<char> &m, size_t target, std::vector<int> &dist
 
 void run(std::string_view buf)
 {
-    auto lines = split_lines(buf);
-    auto m = Matrix<char>::from_lines(lines);
+    auto m = Matrix<char>::from_lines(split_lines(buf));
 
     // Find the start and end point (at least for my input, the start point
     // isn't at the top left as in the example).
-    size_t start_index = 0, end_index = 0;
-    for (size_t i = 0; i < m.size(); i++) {
-        if (m.data[i] == 'S')
-            start_index = i;
-        else if (m.data[i] == 'E')
-            end_index = i;
-    }
+    size_t start_index = std::ranges::find(m.all(), 'S') - m.all().begin();
+    size_t end_index = std::ranges::find(m.all(), 'E') - m.all().begin();
     m.data[start_index] = 'a';
     m.data[end_index] = 'z';
 
+    std::vector<uint32_t> dist(m.size(), 100000);
+
     // Part 1:
-    {
-        std::vector<int> dist(m.size(), 100000);
-        dist[start_index] = 0;
-        fmt::print("{}\n", dijkstra(m, end_index, dist));
-    }
+    dist[start_index] = 0;
+    fmt::print("{}\n", dijkstra(m, end_index, dist));
 
     // Part 2:
     {
-        std::vector<int> dist(m.size(), 100000);
+        dist.assign(m.size(), 100000);
 
         // Set the distance of all 'a' points to 0. In effect, this causes
         // Dijkstra's algorithm to consider them all as starting points
