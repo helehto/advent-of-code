b46c56016a035b3032cda514bcfe35fa90ebf4b6
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-07-08 03:13:26 +0200

    2020/15: Optimize by splitting table into dense/sparse parts
    Far enough into the sequence, it degenerates into unpredictable indexing
    into the 30 million entry array, virtually guaranteeing a cache miss for
    every step.
    
    When all is said and done, roughly 90% of entries there are untouched.
    Introduce a hash table for storing entries at larger indices, while
    keeping a smaller array for lower indices due to its random-access
    performance. The hard-coded cut-off of 2,000,000 here seemed to work
    best.

--- 2020/15.cc
+++ 2020/15.cc
@@ -1,11 +1,26 @@
 #include "common.h"
+#include "dense_map.h"
 
 namespace aoc_2020_15 {
 
-static int32_t step(int32_t turn, int32_t last, std::vector<int32_t> &seen)
+/// The cut-off index after which value are cached in a dense_map instead.
+constexpr int32_t sparse_cutoff = 2'000'000;
+
+static int32_t step(int32_t turn,
+                    int32_t last,
+                    std::vector<int32_t> &seen_dense,
+                    dense_map<int32_t, int32_t> &seen_sparse)
 {
-    const int32_t t = std::exchange(seen[last], turn - 1);
-    return t >= 0 ? turn - t - 1 : 0;
+    if (last < sparse_cutoff) {
+        const int32_t t = std::exchange(seen_dense[last], turn - 1);
+        return t >= 0 ? turn - t - 1 : 0;
+    } else {
+        if (auto [it, inserted] = seen_sparse.emplace(last, turn - 1); !inserted) {
+            const int32_t t = std::exchange(it->second, turn - 1);
+            return turn - t - 1;
+        }
+        return 0;
+    }
 }
 
 void run(std::string_view buf)
@@ -13,18 +28,21 @@ void run(std::string_view buf)
     small_vector<int32_t> init;
     find_numbers(buf, init);
 
-    std::vector<int32_t> seen(30'000'000, -1);
+    std::vector<int32_t> seen_dense(sparse_cutoff, -1);
     for (size_t i = 0; i < init.size() - 1; i++)
-        seen[init[i]] = i + 1;
+        seen_dense[init[i]] = i + 1;
+
+    dense_map<int32_t, int32_t> seen_sparse;
+    seen_sparse.reserve(1'000'000);
 
     int32_t last = init.back();
     int32_t turn = init.size() + 1;
     for (; turn <= 2020; turn++)
-        last = step(turn, last, seen);
+        last = step(turn, last, seen_dense, seen_sparse);
     fmt::print("{}\n", last);
 
     for (; turn <= 30'000'000; turn++)
-        last = step(turn, last, seen);
+        last = step(turn, last, seen_dense, seen_sparse);
     fmt::print("{}\n", last);
 }
 
