89787fe6f064a8dffa602306f9c0d40f26f0ba22
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-07-08 17:36:15 +0200

    2024/6: Optimize by replacing visited hash table with a grid

--- 2024/6.cc
+++ 2024/6.cc
@@ -7,24 +7,17 @@ enum { N, E, S, W };
 constexpr Vec2i dxdy[] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
 
 struct State {
-    uint16_t x : 15;
-    uint16_t y : 14;
-    uint16_t dir : 2;
+    uint8_t x;
+    uint8_t y;
+    uint8_t dir;
 
     constexpr bool operator==(const State &) const = default;
-
-    struct Hasher {
-        size_t operator()(const State &state) const
-        {
-            return _mm_crc32_u32(0, std::bit_cast<uint32_t>(state));
-        }
-    };
 };
 
 static std::pair<std::vector<State>, size_t> walk1(const Matrix<char> &grid, Vec2u16 p)
 {
     uint16_t dir = N;
-    std::vector<State> visited{State{p.x, p.y, dir}};
+    std::vector<State> visited{State(p.x, p.y, dir)};
     dense_set<Vec2u16> unique{p};
 
     while (true) {
@@ -37,7 +30,7 @@ static std::pair<std::vector<State>, size_t> walk1(const Matrix<char> &grid, Vec
             q = p + dxdy[dir];
         }
 
-        visited.push_back(State{q.x, q.y, dir});
+        visited.push_back(State(q.x, q.y, dir));
         unique.insert(q);
         p = q;
     }
@@ -47,13 +40,15 @@ static std::pair<std::vector<State>, size_t> walk1(const Matrix<char> &grid, Vec
 
 static bool walk2(const Matrix<char> &grid,
                   Vec2u16 p,
-                  dense_set<State, State::Hasher> &visited,
+                  std::array<Matrix<bool>, 4> &visited,
                   dense_set<Vec2u16> *out = nullptr)
 {
     uint16_t dir = N;
-    visited.clear();
-    visited.reserve(grid.rows * grid.cols);
-    visited.insert(State{p.x, p.y, dir});
+
+    for (Matrix<bool> &m : visited)
+        std::ranges::fill(m.all(), false);
+
+    visited[dir](p) = true;
 
     while (true) {
         auto q = p + dxdy[dir];
@@ -65,8 +60,9 @@ static bool walk2(const Matrix<char> &grid,
             q = p + dxdy[dir];
         }
 
-        if (!visited.insert(State{q.x, q.y, dir}).second)
+        if (visited[dir](q))
             return false;
+        visited[dir](q) = true;
 
         p = q;
     }
@@ -74,8 +70,12 @@ static bool walk2(const Matrix<char> &grid,
     if (out) {
         out->clear();
         out->reserve(visited.size());
-        for (State state : visited)
-            out->insert({state.x, state.y});
+        for (size_t dir = 0; dir < 4; ++dir) {
+            for (auto p : visited[dir].ndindex<uint16_t>()) {
+                if (visited[dir](p))
+                    out->insert(p);
+            }
+        }
     }
 
     return true;
@@ -85,6 +85,8 @@ void run(std::string_view buf)
 {
     auto lines = split_lines(buf);
     auto grid = Matrix<char>::from_lines(lines);
+    ASSERT(grid.rows < 256);
+    ASSERT(grid.cols < 256);
 
     Vec2u16 start{};
     for (auto p : grid.ndindex<uint16_t>()) {
@@ -98,7 +100,9 @@ void run(std::string_view buf)
     fmt::print("{}\n", n_route_unique);
 
     dense_set<Vec2u16> visited_points;
-    dense_set<State, State::Hasher> visited_states;
+    std::array<Matrix<bool>, 4> visited_states;
+    for (auto &v : visited_states)
+        v = Matrix<bool>(grid.rows, grid.cols);
     walk2(grid, start, visited_states, &visited_points);
 
     int n_loops = 0;
