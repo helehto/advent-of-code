c18e81b40c2c1111885a7d9caa3ab787aa096f61
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-07-14 23:33:51 +0200

    2024/9: Optimize
     * Part 1: Vectorize construction of the disk
     * Part 2: Find appropriate gaps via heaps instead of brute-force

--- 2024/9.cc
+++ 2024/9.cc
@@ -3,46 +3,24 @@
 
 namespace aoc_2024_9 {
 
-static int64_t checksum(std::span<const int> disk)
-{
-    int64_t result = 0;
-
-    for (size_t i = 0; i < disk.size(); ++i)
-        if (disk[i] >= 0)
-            result += i * disk[i];
-
-    return result;
-}
-
-static std::vector<int>
-build_disk(std::string_view line,
-           std::vector<std::pair<uint32_t, uint32_t>> *files = nullptr)
-{
-    std::vector<int> disk;
-    disk.reserve(10 * line.size());
-
-    for (size_t i = 0; i < line.size(); ++i) {
-        if (line[i] >= '0' && line[i] <= '9') {
-            const int n = line[i] - '0';
-            const int f = i % 2 == 0 ? i / 2 : -1;
-
-            if (i % 2 == 0) {
-                if (files)
-                    files->emplace_back(disk.size(), n);
-            }
-            for (int j = 0; j < n; ++j)
-                disk.push_back(f);
-        }
-    }
-
-    return disk;
-}
-
 static int64_t part1(std::string_view line)
 {
-    auto disk = build_disk(line);
+    size_t disk_size = 0;
+    for (size_t i = 0; i < line.size(); ++i)
+        disk_size += line[i] - '0';
 
-    for (size_t i = 0, j = disk.size() - 1;;) {
+    auto disk = std::make_unique_for_overwrite<int16_t[]>(disk_size + 16);
+    size_t offset = 0;
+    for (size_t i = 0; i < line.size(); ++i) {
+        const int n = line[i] - '0';
+        const int f = i % 2 == 0 ? i / 2 : -1;
+        _mm256_storeu_si256((__m256i *)&disk[offset], _mm256_set1_epi16(f));
+        offset += n;
+    }
+
+    size_t i = 0;
+    size_t j = disk_size - 1;
+    while (true) {
         while (disk[i] >= 0)
             i++;
         while (disk[j] < 0)
@@ -50,45 +28,72 @@ static int64_t part1(std::string_view line)
         if (i >= j)
             break;
         disk[i] = disk[j];
-        disk[j] = -1;
+        i++;
         j--;
     }
 
-    return checksum(disk);
+    uint64_t checksum = 0;
+    for (size_t k = 0; k <= j; ++k)
+        if (disk[k] >= 0)
+            checksum += k * disk[k];
+
+    return checksum;
 }
 
 static int64_t part2(std::string_view line)
 {
     std::vector<std::pair<uint32_t, uint32_t>> files;
-    auto disk = build_disk(line, &files);
+    files.reserve(line.size() / 2);
 
-    for (size_t id = files.size(); id--;) {
-        auto [file_pos, file_len] = files[id];
-        size_t i = 0;
+    std::array<std::vector<uint32_t>, 10> gaps;
+    for (size_t i = 0, offset = 0; i < line.size(); ++i) {
+        const int n = line[i] - '0';
+        if (i % 2 == 0)
+            files.emplace_back(offset, n);
+        else
+            gaps[n].push_back(offset);
 
-        while (true) {
-            while (i < file_pos && disk[i] >= 0)
-                i++;
-            if (i >= file_pos || i >= disk.size())
-                goto skip;
-
-            auto j = i + 1;
-            while (disk[j] < 0)
-                j++;
-            if (j - i >= file_len)
-                break;
-
-            i = j + 1;
-        }
-
-        std::ranges::fill(disk.begin() + i, disk.begin() + i + file_len, id);
-        std::ranges::fill(disk.begin() + file_pos, disk.begin() + file_pos + file_len,
-                          -1);
-
-    skip:;
+        offset += n;
     }
 
-    return checksum(disk);
+    for (std::vector<uint32_t> &g : gaps) {
+        g.push_back(UINT32_MAX);
+        std::ranges::make_heap(g, std::greater<>());
+    }
+
+    for (size_t id = files.size(); id--;) {
+        auto &[file_pos, file_len] = files[id];
+
+        size_t gap_pos = SIZE_MAX;
+        size_t gap_len;
+        for (size_t len = 9; len >= file_len; --len) {
+            if (auto &g = gaps[len]; g.front() < gap_pos) {
+                gap_pos = g.front();
+                gap_len = len;
+            }
+        }
+
+        if (gap_pos > file_pos)
+            continue; // either no gap large enough, or would move file forward
+
+        file_pos = gap_pos;
+        std::ranges::pop_heap(gaps[gap_len], std::greater<>());
+        gaps[gap_len].pop_back();
+
+        if (int new_len = gap_len - file_len) {
+            const int new_pos = gap_pos + file_len;
+            gaps[new_len].push_back(new_pos);
+            std::ranges::push_heap(gaps[new_len], std::greater<>());
+        }
+    }
+
+    int64_t checksum = 0;
+    for (size_t id = 0; id < files.size(); ++id) {
+        const auto [pos, len] = files[id];
+        checksum += id * len * (2 * pos + len - 1) / 2;
+    }
+
+    return checksum;
 }
 
 void run(std::string_view buf)
