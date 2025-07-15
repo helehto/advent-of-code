b90a9a70142a9d6ed673ad4d0c90896a4592576d
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-07-15 10:01:42 +0200

    2019/3: Optimize by efficiently counting polyline intersections

--- 2019/3.cc
+++ 2019/3.cc
@@ -6,58 +6,78 @@ namespace aoc_2019_3 {
 void run(std::string_view buf)
 {
     auto lines = split_lines(buf);
-    dense_map<Vec2i16, int8_t> visited;
-    dense_map<Vec2i16, int> steps_maps[2];
 
-    std::vector<std::string_view> moves;
+    std::vector<std::string_view> tmp;
+    std::array<small_vector<Vec2i16, 512>, 2> moves;
     for (size_t i = 0; i < 2; i++) {
-        split(lines[i], moves, ',');
-
-        Vec2i16 p{0, 0};
-        int steps = 0;
-        for (std::string_view w : moves) {
+        for (std::string_view w : split(lines[i], tmp, ',')) {
             int n = 0;
             std::from_chars(w.data() + 1, w.data() + w.size(), n);
+            ASSERT(n != 0);
 
-            Vec2i16 d{};
             if (w[0] == 'U')
-                d.y = 1;
-            if (w[0] == 'D')
-                d.y = -1;
-            if (w[0] == 'L')
-                d.x = -1;
-            if (w[0] == 'R')
-                d.x = 1;
+                moves[i].emplace_back(0, n);
+            else if (w[0] == 'D')
+                moves[i].emplace_back(0, -n);
+            else if (w[0] == 'L')
+                moves[i].emplace_back(-n, 0);
+            else
+                moves[i].emplace_back(n, 0);
+        }
+    }
 
-            for (int j = 0; j < n; j++) {
-                p += d;
-                visited[p] |= 1 << i;
-                steps_maps[i].emplace(p, ++steps);
+    // Turn the two paths into rectilinear polylines, separated into horizontal
+    // and vertical edges:
+    std::array<small_vector<std::tuple<int, Vec2i64, Vec2i64>, 512>, 2> h_edges;
+    std::array<small_vector<std::tuple<int, Vec2i64, Vec2i64>, 512>, 2> v_edges;
+    std::array<small_vector<int, 512>, 2> steps_to_point;
+    for (size_t i = 0; i < 2; i++) {
+        Vec2i16 p{};
+        steps_to_point[i].push_back(0);
+        for (size_t j = 0; const Vec2i16 d : moves[i]) {
+            auto q = p + d;
+            auto &edges = d.x != 0 ? h_edges[i] : v_edges[i];
+            edges.emplace_back(j, p.cast<int64_t>(), q.cast<int64_t>());
+            steps_to_point[i].push_back(manhattan(d) + steps_to_point[i].back());
+            p = q;
+            ++j;
+        }
+    }
+
+    // Find the intersection points between the two polylines. Since they are
+    // rectilinear and parallel edges cannot intersect by definition, we need
+    // only check horizontal against vertical edges for the two polygons.
+    int min_dist = INT_MAX;
+    int min_combined_steps = INT_MAX;
+    for (size_t i = 0; i < 2; ++i) {
+        for (const auto &[j, p1, p2] : h_edges[i]) {
+            for (const auto &[k, q1, q2] : v_edges[1 - i]) {
+                // Ignore the initial intersection at (0,0).
+                if (j == 0 && k == 0) [[unlikely]]
+                    continue;
+
+                const int64_t dx = p1.x - p2.x;
+                const int64_t dy = q1.y - q2.y;
+                const int64_t cx = (q1.x * q2.y - q2.x * q1.y) * dx;
+                const int64_t cy = (p1.x * p2.y - p2.x * p1.y) * dy;
+                const Vec2i64 c = {-cx / (dx * dy), -cy / (dx * dy)};
+
+                const bool intersect =
+                    c.x >= std::min(p1.x, p2.x) && c.x <= std::max(p1.x, p2.x) &&
+                    c.y >= std::min(q1.y, q2.y) && c.y <= std::max(q1.y, q2.y);
+
+                if (intersect) {
+                    min_dist = std::min<int64_t>(min_dist, manhattan(c));
+                    const int steps = (steps_to_point[0][j] + std::abs(p1.x - c.x)) +
+                                      (steps_to_point[1][k] + std::abs(q1.y - c.y));
+                    min_combined_steps = std::min<int>(min_combined_steps, steps);
+                }
             }
         }
     }
 
-    std::vector<Vec2i16> crossings;
-    crossings.reserve(visited.size());
-    for (auto &[p, count] : visited) {
-        if (count == 3)
-            crossings.push_back(p);
-    }
-
-    Vec2i16 minp = {1000, 1000};
-    for (Vec2i16 &p : crossings) {
-        if (manhattan(p) < manhattan(minp))
-            minp = p;
-    }
-
-    int minsteps = INT_MAX;
-    for (Vec2i16 &p : crossings) {
-        int steps = steps_maps[0][p] + steps_maps[1][p];
-        if (steps < minsteps)
-            minsteps = steps;
-    }
-
-    fmt::print("{}\n{}\n", manhattan(minp), minsteps);
+    fmt::print("{}\n", min_dist);
+    fmt::print("{}\n", min_combined_steps);
 }
 
 }
