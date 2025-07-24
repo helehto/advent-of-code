6250256f079d948782ddfd5294c93b18b48aa680
Henrik Lehtonen <94722854+helehto@users.noreply.github.com>
2025-07-24 18:15:26 +0200

    2021/10: Optimize by replacing branches with tables

--- 2021/10.cc
+++ 2021/10.cc
@@ -2,60 +2,65 @@
 
 namespace aoc_2021_10 {
 
+constexpr auto part1_score_table = [] {
+    std::array<int, 256> t{};
+    t[')'] = 3;
+    t[']'] = 57;
+    t['}'] = 1197;
+    t['>'] = 25137;
+    return t;
+}();
+
+constexpr static auto part2_score_table = [] {
+    std::array<int, 256> t{};
+    t[')'] = 1;
+    t[']'] = 2;
+    t['}'] = 3;
+    t['>'] = 4;
+    return t;
+}();
+
+constexpr static auto matching_pairs = [] {
+    std::array<char, 256> t{};
+    t['('] = ')';
+    t['['] = ']';
+    t['{'] = '}';
+    t['<'] = '>';
+    return t;
+}();
+
 void run(std::string_view buf)
 {
     int score1 = 0;
-    std::vector<char> stack;
-    std::vector<uint64_t> scores2;
+    std::array<char, 64> stack;
+    small_vector<uint64_t, 64> scores2;
 
     for (std::string_view s : split_lines(buf)) {
-        stack.clear();
-        bool incorrect = false;
+        char *p = stack.data();
+        int mismatch_score = 0;
 
-        for (char c : s) {
-            if (c == '(') {
-                stack.push_back(')');
-            } else if (c == '[') {
-                stack.push_back(']');
-            } else if (c == '{') {
-                stack.push_back('}');
-            } else if (c == '<') {
-                stack.push_back('>');
+        for (const char c : s) {
+            if (const char m = matching_pairs[c]) {
+                *p++ = m;
             } else {
-                if (c != stack.back()) {
-                    if (c == ')')
-                        score1 += 3;
-                    else if (c == ']')
-                        score1 += 57;
-                    else if (c == '}')
-                        score1 += 1197;
-                    else if (c == '>')
-                        score1 += 25137;
-                    incorrect = true;
-                }
-                stack.pop_back();
+                if (c != *--p)
+                    mismatch_score += part1_score_table[static_cast<uint8_t>(c)];
             }
         }
 
-        if (!stack.empty() && !incorrect) {
+        if (mismatch_score == 0) {
             uint64_t score2 = 0;
-            for (size_t i = stack.size(); i--;) {
-                score2 = 5 * score2;
-                if (stack[i] == ')')
-                    score2 += 1;
-                else if (stack[i] == ']')
-                    score2 += 2;
-                else if (stack[i] == '}')
-                    score2 += 3;
-                else if (stack[i] == '>')
-                    score2 += 4;
-            }
-            scores2.push_back(score2);
+            for (size_t i = p - stack.data(); i--;)
+                score2 = 5 * score2 + part2_score_table[stack[i]];
+            if (score2)
+                scores2.push_back(score2);
         }
+
+        score1 += mismatch_score;
     }
 
     fmt::print("{}\n", score1);
-    std::sort(begin(scores2), end(scores2));
+    std::ranges::nth_element(scores2, scores2.begin() + scores2.size() / 2);
     fmt::print("{}\n", scores2[scores2.size() / 2]);
 }
 
