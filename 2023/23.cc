197ad6756528b2fe8f038e420583013d452e29fc
Henrik Lehtonen <eigengrau@vm86.se>
2025-08-15 00:10:34 +0200

    2023/23: Optimize via multithreading and not exploring dead ends
    As a side effect, this now solves part 1 and 2 in parallel by submitting
    both as root tasks to a work-stealing queue (though part 2 obviously
    takes much longer to solve than part 1).

--- 2023/23.cc
+++ 2023/23.cc
@@ -1,114 +1,211 @@
 #include "common.h"
-#include "dense_set.h"
-#include <algorithm>
-#include <vector>
+#include "dense_map.h"
+#include <random>
+#include <thread>
 
 namespace aoc_2023_23 {
 
 struct alignas(16) Node {
     uint8_t n_edges = 0;
+    int8_t distance_from_goal = -1;
     uint8_t edges[4];
     uint16_t weights[4];
 };
+static_assert(sizeof(Node) == 16);
 
 struct Graph {
-    std::vector<Node> nodes;
+    inplace_vector<Node, 64> nodes;
     dense_map<Vec2z, size_t> node_map;
     uint8_t start_index;
     uint8_t goal_index;
+
+    size_t get_node(Vec2z p)
+    {
+        if (auto it = node_map.find(p); it != node_map.end()) {
+            return it->second;
+        } else {
+            nodes.emplace_back();
+            node_map.emplace(p, nodes.size() - 1);
+            return nodes.size() - 1;
+        }
+    }
+
+    void add_edge(Vec2z p, Vec2z q, int weight)
+    {
+        auto add_unique = [&](Node &node, uint8_t v) {
+            for (size_t i = 0; i < node.n_edges; i++) {
+                if (node.edges[i] == v)
+                    return;
+            }
+            node.edges[node.n_edges] = v;
+            node.weights[node.n_edges] = weight;
+            node.n_edges++;
+        };
+
+        const auto pn = get_node(p);
+        const auto qn = get_node(q);
+        add_unique(nodes[pn], qn);
+        add_unique(nodes[qn], pn | 0x80);
+    }
 };
 
-static constexpr uint64_t bit(int i)
+struct alignas(16) State {
+    uint64_t visited_mask;
+    uint8_t node_index;
+    uint16_t total_weight;
+    bool ignore_slopes;
+};
+static_assert(sizeof(State) == 16);
+
+// Implementation of a concurrent lock-free deque.
+//
+// See "Dynamic Circular Work-Stealing Deque" by Chase and Lev (2005) for the
+// initial paper describing this algorithm. This implementation follows the
+// pseudocode in "Correct and Efficient Work-Stealing for Weak Memory Models"
+// by Lê et al (2013).
+struct WorkQueue {
+    static constexpr size_t buffer_size = 64;
+    static constexpr size_t mask = buffer_size - 1;
+
+    // Accessed by owner threads (push/pop) *and* thieves (steal):
+    alignas(64) std::array<State, buffer_size> buffer;
+
+    // Accessed by owner threads:
+    alignas(64) std::atomic<uint64_t> bottom = 0;
+
+    // Accessed by thieves:
+    alignas(64) std::atomic<uint64_t> top = 0;
+
+    bool pop(State &result)
+    {
+        const uint64_t b = bottom.load(std::memory_order_relaxed) - 1;
+        bottom.store(b, std::memory_order_relaxed);
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+
+        uint64_t t = top.load(std::memory_order_relaxed);
+        if (static_cast<int64_t>(t) <= static_cast<int64_t>(b)) {
+            result = buffer[b & mask];
+            if (t == b) {
+                // Single last element in queue.
+                const bool ok = top.compare_exchange_strong(
+                    t, t + 1, std::memory_order_seq_cst, std::memory_order_relaxed);
+                bottom.store(b + 1, std::memory_order_relaxed);
+                return ok;
+            }
+        } else {
+            // Empty queue.
+            bottom.store(b + 1, std::memory_order_relaxed);
+            return false;
+        }
+
+        return true;
+    }
+
+    void push(const State &state)
+    {
+        const uint64_t b = bottom.load(std::memory_order_relaxed);
+        const uint64_t t = top.load(std::memory_order_acquire);
+        ASSERT(static_cast<int64_t>(b) - static_cast<int64_t>(t) <
+               static_cast<int64_t>(buffer_size));
+        buffer[b & mask] = state;
+        std::atomic_thread_fence(std::memory_order_release);
+        bottom.store(b + 1, std::memory_order_relaxed);
+    }
+
+    bool steal(State &result)
+    {
+        uint64_t t = top.load(std::memory_order_acquire);
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        const uint64_t b = bottom.load(std::memory_order_acquire);
+        if (t >= b)
+            return false;
+
+        result = buffer[t & mask];
+        return top.compare_exchange_strong(t, t + 1, std::memory_order_seq_cst,
+                                           std::memory_order_relaxed);
+    }
+};
+
+template <typename T>
+static void atomic_store_max(std::atomic<T> &a, size_t b)
 {
-    return UINT64_C(1) << i;
+    auto value = a.load(std::memory_order_relaxed);
+    while (b > value && !a.compare_exchange_weak(value, b))
+        ;
 }
 
-template <bool IncludePredecessors>
-__attribute__((noinline)) static int64_t search(const Graph &graph)
+static void search(size_t thread_id,
+                   size_t n_threads,
+                   const Graph &graph,
+                   std::atomic<size_t> &n_thieves,
+                   std::atomic<size_t> *solutions,
+                   WorkQueue *all_queues)
 {
-    struct State {
-        uint64_t visited_mask;
-        const Node *node;
-        uint16_t total_weight;
-    };
+    WorkQueue &queue = all_queues[thread_id];
+    State u;
 
-    auto stack = std::make_unique<State[]>(16384);
-    State *p = stack.get();
+    small_vector<size_t, 32> victim_order;
+    for (size_t i = 0; i < n_threads; ++i)
+        if (i != thread_id)
+            victim_order.push_back(i);
 
-    *p++ = State{
-        .visited_mask = 0,
-        .node = &graph.nodes[graph.start_index],
-        .total_weight = 0,
-    };
+    // Randomize the order in which each thief tries to steal work from the
+    // other threads, to avoid a "convoy" of thieves hammering the queues of a
+    // sequence of threads in a deterministic way.
+    std::ranges::shuffle(victim_order, std::minstd_rand(thread_id));
 
-    uint16_t max_length = 0;
-
-    while (p != stack.get()) {
-        auto [visited_mask, node, total_weight] = *--p;
-        const auto this_mask = bit(node - graph.nodes.data());
-
-        if (node == &graph.nodes[graph.goal_index]) {
-            max_length = std::max(max_length, total_weight);
+    while (queue.pop(u)) {
+    restart_with_stolen_work:
+        auto [visited_mask, node_index, total_weight, ignore_slopes] = u;
+        if (node_index == graph.goal_index) {
+            auto &sol = ignore_slopes ? solutions[1] : solutions[0];
+            atomic_store_max(sol, total_weight);
             continue;
         }
 
-        for (size_t i = 0; i < node->n_edges; ++i) {
-            const auto next = node->edges[i] & ~0x80;
-            const auto weight = node->weights[i];
+        const Node &node = graph.nodes[node_index];
+        for (size_t i = 0; i < node.n_edges; ++i) {
+            if (!ignore_slopes && (node.edges[i] & 0x80) != 0)
+                continue;
 
-            if constexpr (!IncludePredecessors) {
-                if (node->edges[i] & 0x80)
-                    continue;
-            }
+            const uint8_t next_index = node.edges[i] & ~0x80;
+            const Node &next = graph.nodes[next_index];
 
-            if ((visited_mask & bit(next)) == 0) {
-                *p++ = State{
-                    .visited_mask = visited_mask | this_mask,
-                    .node = &graph.nodes[next],
-                    .total_weight = static_cast<uint16_t>(total_weight + weight),
-                };
-            }
+            // Avoid dead ends: the graph is a variant of a rook's graph
+            // (https://en.wikipedia.org/wiki/Rook%27s_graph), but where only
+            // adjacent nodes on the board are connected with an edge.
+            if (node.n_edges == 3 && next.n_edges == 3 &&
+                next.distance_from_goal > node.distance_from_goal)
+                continue;
+
+            if ((visited_mask & (UINT64_C(1) << next_index)) == 0)
+                queue.push(State{
+                    .visited_mask = visited_mask | (UINT64_C(1) << node_index),
+                    .node_index = next_index,
+                    .total_weight = static_cast<uint16_t>(total_weight + node.weights[i]),
+                    .ignore_slopes = ignore_slopes,
+                });
         }
     }
 
-    return max_length;
-}
-
-static size_t get_node(Graph &graph, Vec2z p)
-{
-    if (auto it = graph.node_map.find(p); it != graph.node_map.end()) {
-        return it->second;
-    } else {
-        graph.nodes.emplace_back();
-        graph.node_map.emplace(p, graph.nodes.size() - 1);
-        return graph.nodes.size() - 1;
-    }
-}
-
-static void add_edge(Graph &graph, Vec2z p, Vec2z q, int weight)
-{
-    auto add_unique = [&](Node &node, uint8_t v) {
-        for (size_t i = 0; i < node.n_edges; i++) {
-            if (node.edges[i] == v)
-                return;
+    // Out of items. If there are only thieves left, we are done; otherwise,
+    // try to steal something from a worker thread.
+    n_thieves.fetch_add(1, std::memory_order_relaxed);
+    while (n_thieves.load(std::memory_order_relaxed) != n_threads) {
+        for (const size_t i : victim_order) {
+            if (all_queues[i].steal(u)) {
+                n_thieves.fetch_sub(1, std::memory_order_relaxed);
+                goto restart_with_stolen_work;
+            }
         }
-        node.edges[node.n_edges] = v;
-        node.weights[node.n_edges] = weight;
-        node.n_edges++;
-    };
-
-    const auto pn = get_node(graph, p);
-    const auto qn = get_node(graph, q);
-    add_unique(graph.nodes[pn], qn);
-    add_unique(graph.nodes[qn], pn | 0x80);
+    }
 }
 
 static Graph build_graph(const Matrix<char> &grid, const Vec2z start, const Vec2z goal)
 {
     Graph graph;
 
-    std::vector<std::pair<Vec2z, Vec2z>> queue{{start, start}};
-    std::vector<Vec2z> neighbors;
+    small_vector<std::pair<Vec2z, Vec2z>, 128> queue{{start, start}};
 
     for (size_t i = 0; i < queue.size(); i++) {
         auto [origin, curr] = queue[i];
@@ -116,16 +213,16 @@ static Graph build_graph(const Matrix<char> &grid, const Vec2z start, const Vec2
 
         for (int weight = 1;; weight++) {
             if (curr == goal) {
-                add_edge(graph, origin, curr, weight);
+                graph.add_edge(origin, curr, weight);
                 break;
             }
 
             // Generate candidate moves to determine if this is a crossing,
             // regardless of icy slopes.
-            neighbors.clear();
+            inplace_vector<Vec2z, 4> neighbors;
             for (auto q : neighbors4(grid, curr))
                 if (q != prev && grid(q) != '#')
-                    neighbors.push_back(q);
+                    neighbors.unchecked_push_back(q);
 
             if (neighbors.size() != 1) {
                 // It's a crossing; filter by icy slopes to get the next possible
@@ -139,7 +236,7 @@ static Graph build_graph(const Matrix<char> &grid, const Vec2z start, const Vec2
                 });
                 ASSERT(!neighbors.empty());
 
-                add_edge(graph, origin, curr, weight);
+                graph.add_edge(origin, curr, weight);
                 for (size_t j = 0; j < neighbors.size(); j++)
                     queue.emplace_back(curr, neighbors[j]);
                 break;
@@ -152,6 +249,25 @@ static Graph build_graph(const Matrix<char> &grid, const Vec2z start, const Vec2
 
     graph.start_index = graph.node_map.at(start);
     graph.goal_index = graph.node_map.at(goal);
+
+    // BFS from goal to find the shortest distance to each node in terms of
+    // number of intersections.
+    {
+        graph.nodes[graph.goal_index].distance_from_goal = 0;
+        inplace_vector<std::pair<Node *, size_t>, 128> queue{
+            {&graph.nodes[graph.goal_index], 0}};
+        for (size_t i = 0; i < queue.size(); ++i) {
+            const auto [u, d] = queue[i];
+            for (size_t j = 0; j < u->n_edges; ++j) {
+                const auto v = &graph.nodes[u->edges[j] & ~0x80];
+                if (v->distance_from_goal < 0) {
+                    queue.emplace_back(v, d + 1);
+                    v->distance_from_goal = d + 1;
+                }
+            }
+        }
+    }
+
     return graph;
 }
 
@@ -175,10 +291,29 @@ void run(std::string_view buf)
     }
 
     auto graph = build_graph(grid, start, goal);
-    ASSERT(graph.nodes.size() < 64);
 
-    fmt::print("{}\n", search<false>(graph) - 1);
-    fmt::print("{}\n", search<true>(graph) - 1);
+    {
+        const int n_threads = std::thread::hardware_concurrency();
+        std::vector<WorkQueue> queues(n_threads);
+
+        // Push root tasks for part 1 and part 2.
+        auto &q = queues[0];
+        q.push(State{.node_index = graph.start_index, .ignore_slopes = false});
+        q.push(State{.node_index = graph.start_index, .ignore_slopes = true});
+
+        std::atomic<size_t> n_thieves = 0;
+        std::array<std::atomic<size_t>, 2> solutions{};
+        {
+            std::vector<std::jthread> threads(n_threads);
+            for (size_t i = 0; i < threads.size(); ++i) {
+                threads[i] =
+                    std::jthread(search, i, n_threads, std::cref(graph),
+                                 std::ref(n_thieves), solutions.data(), queues.data());
+            }
+        }
+
+        fmt::print("{}\n{}\n", solutions[0].load() - 1, solutions[1].load() - 1);
+    }
 }
 
 }
