8c7729acd98b589dad8af606aba085fcd7fd4894
Henrik Lehtonen <eigengrau@vm86.se>
2025-12-04 17:11:43 +0100

    2025/2: Optimize
      Year    Day    Iterations    Mean (μs)    σ (μs)    10% (μs)    90% (μs)
    ------  -----  ------------  -----------  --------  ----------  ----------
      2025      2        739797      6.02944  0.033344       5.991       6.072

--- 2025/2.cc
+++ 2025/2.cc
@@ -3,72 +3,90 @@
 
 namespace aoc_2025_2 {
 
-static small_vector<size_t, 16> proper_divisors(size_t n)
-{
-    small_vector<size_t, 16> result;
-    for (size_t k = 1; k < n; ++k) {
-        if (n % k == 0)
-            result.push_back(k);
+/// To construct a repeating number with `n` repeated `k` times, we can simply
+/// multiply `n` by an appropriate factor, e.g.:
+///
+///     7 * 111 = 777
+///     23 * 10101010101 = 232323232323
+///     12345 * 10000100001 = 123451234512345
+///
+/// This table contains precomputed factors for all relevant `n` and `k`.
+constexpr auto repeater_factors = [] consteval {
+    std::array<std::array<uint64_t, 18>, 18> result{};
+    for (uint64_t i = 1; i < 18; ++i) {
+        for (uint64_t j = 1; j < 18; ++j) {
+            // Some of these entries will overflow uint64_t, but we don't care
+            // since they won't be used.
+            result[i][j] = result[i][j - 1] * pow10i[i] + 1;
+        }
     }
     return result;
-}
+}();
 
-constexpr size_t make_repeating(const size_t n, const size_t times)
+static uint64_t sum_unique(small_vector_base<uint64_t> &r)
 {
-    size_t result = 0;
-    for (size_t i = 0; i < times; ++i) {
-        result = result * pow10i[digit_count_base10(n)] + n;
-    }
-    return result;
+    std::ranges::sort(r);
+    auto w = std::ranges::unique(r);
+    r.erase(w.begin(), w.end());
+    return std::ranges::fold_left(r, 0, λab(a + b));
 }
-static_assert(make_repeating(123, 3) == 123123123);
 
-static size_t count_reps_2(size_t a, size_t b)
+static const inplace_vector<int8_t, 5> proper_divisors[] = {
+    /* 0  */ {},
+    /* 1  */ {},
+    /* 2  */ {1},
+    /* 3  */ {1},
+    /* 4  */ {1, 2},
+    /* 5  */ {1},
+    /* 6  */ {1, 2, 3},
+    /* 7  */ {1},
+    /* 8  */ {1, 2, 4},
+    /* 9  */ {1, 3},
+    /* 10 */ {1, 2, 5},
+    /* 11 */ {1},
+    /* 12 */ {1, 2, 3, 4, 6},
+    /* 13 */ {1},
+    /* 14 */ {1, 2, 7},
+    /* 15 */ {1, 3, 5},
+    /* 16 */ {1, 2, 4, 8},
+    /* 17 */ {1},
+    /* 18 */ {1, 2, 3, 6, 9},
+    /* 19 */ {1},
+};
+
+template <int Part>
+static size_t count_reps_any(small_vector_base<uint64_t> &buffer, size_t a, size_t b)
 {
     const size_t na = digit_count_base10(a);
     const size_t nb = digit_count_base10(b);
 
-    dense_set<size_t> q;
-    q.reserve(16);
+    buffer.clear();
 
     for (size_t n = na; n <= nb; ++n) {
-        if (n % 2 != 0)
-            continue;
+        const uint64_t aa = std::clamp(a, pow10i[n - 1], pow10i[n]);
+        const uint64_t bb = std::clamp(b, pow10i[n - 1], pow10i[n]);
 
-        const size_t d = n / 2;
+        inplace_vector<int8_t, 5> divs;
+        if constexpr (Part == 1) {
+            if (n % 2 == 0)
+                divs.push_back(n / 2);
+        } else {
+            divs = proper_divisors[n];
+        }
 
-        for (size_t k = pow10i[d - 1]; k < pow10i[d]; ++k) {
-            const size_t rep = make_repeating(k, n / d);
-            if (a <= rep && rep <= b)
-                q.insert(rep);
+        for (const int d : divs) {
+            const uint64_t r = repeater_factors[d][n / d];
+            const uint64_t lo = (aa + r - 1) / r;
+            const uint64_t hi = (bb - 1) / r;
+
+            // TODO: Use the inclusion-exclusion principle here to avoid
+            // double-counting, instead of having to prune later...
+            for (uint64_t k = lo; k <= hi; ++k)
+                buffer.push_back(k * r);
         }
     }
 
-    return std::ranges::fold_left(q, 0, λab(a + b));
-}
-
-static size_t count_reps_any(size_t a, size_t b)
-{
-    const size_t na = digit_count_base10(a);
-    const size_t nb = digit_count_base10(b);
-
-    dense_set<size_t> q;
-    q.reserve(16);
-
-    for (size_t n = na; n <= nb; ++n) {
-        for (const size_t d : proper_divisors(n)) {
-            for (size_t k = pow10i[d - 1]; k < pow10i[d]; ++k) {
-                const size_t rep = make_repeating(k, n / d);
-                if (a <= rep && rep <= b) {
-                    // Use a set here to avoid double-counting (e.g. 11111
-                    // counts for d=1, d=2 and d=5).
-                    q.insert(rep);
-                }
-            }
-        }
-    }
-
-    return std::ranges::fold_left(q, 0, λab(a + b));
+    return sum_unique(buffer);
 }
 
 void run(std::string_view buf)
@@ -79,11 +97,11 @@ void run(std::string_view buf)
     size_t part1 = 0;
     size_t part2 = 0;
 
+    small_vector<uint64_t, 256> tmp;
     for (auto r : ranges) {
         auto [a, b] = find_numbers_n<size_t, 2>(r);
-
-        part1 += count_reps_2(a, b);
-        part2 += count_reps_any(a, b);
+        part1 += count_reps_any<1>(tmp, a, b);
+        part2 += count_reps_any<2>(tmp, a, b);
     }
 
     fmt::print("{}\n{}\n", part1, part2);
