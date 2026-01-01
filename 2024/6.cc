6989e15c0460d79276f6da87e73f588ff14a7564
Henrik Lehtonen <eigengrau@vm86.se>
2026-01-02 00:56:13 +0100

    2024/6: Optimize via multithreading
      Year    Day    n0    n1    min t₀    min t₁      Δmin  Δmin%      avg t₀    avg t₁      Δavg  Δavg%
    ------  -----  ----  ----  --------  --------  --------  -------  --------  --------  --------  -------
      2024      6    41  1520   18756.3   821.116  -17935.2  -95.6%      19154   1202.47  -17951.5  -93.7%

--- 2024/6.cc
+++ 2024/6.cc
@@ -1,120 +1,161 @@
 #include "common.h"
-#include "dense_set.h"
+#include "thread_pool.h"
 
 namespace aoc_2024_6 {
 
 enum { N, E, S, W };
-constexpr Vec2i dxdy[] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
 
-struct State {
-    uint8_t x;
-    uint8_t y;
-    uint8_t dir;
+struct alignas(4) State {
+    uint16_t offset = 0;
+    uint16_t dir = N;
+};
+static_assert(sizeof(State) == 4);
 
-    constexpr bool operator==(const State &) const = default;
+struct Bitset {
+    std::vector<uint64_t> blocks;
+
+    Bitset() noexcept = default;
+
+    Bitset(size_t n_bits) noexcept
+        : blocks((n_bits + 63) / 64, 0)
+    {
+    }
+
+    void clear() noexcept { memset(blocks.data(), 0, blocks.size() * sizeof(blocks[0])); }
+
+    bool test_and_set(size_t i) noexcept
+    {
+        size_t &block = blocks[i / 64];
+        const uint64_t mask = UINT64_C(1) << (i & 63);
+        const bool was_set = (block & mask) != 0;
+        block |= mask;
+        return was_set;
+    }
+
+    void for_each_set_bit(std::invocable<size_t> auto &&fn) const noexcept
+    {
+        for (size_t b = 0; b < blocks.size(); ++b)
+            for (uint64_t m = blocks[b]; m; m &= m - 1)
+                fn(b * 64 + std::countr_zero(m));
+    }
 };
 
-static std::pair<std::vector<State>, size_t> walk1(MatrixView<const char> grid, Vec2u16 p)
-{
-    uint16_t dir = N;
-    std::vector<State> visited{State(p.x, p.y, dir)};
-    dense_set<Vec2u16> unique{p};
+constexpr char pad_value = '@';
 
-    while (true) {
-        auto q = p + dxdy[dir];
-        if (!grid.in_bounds(q))
+static bool walk(MatrixView<const char> grid,
+                 const State start,
+                 std::invocable<State &&> auto &&visit)
+{
+    const ssize_t stride_by_direction[] = {
+        -static_cast<ssize_t>(grid.cols), // N
+        1,                                // E
+        static_cast<ssize_t>(grid.cols),  // S
+        -1,                               // W
+    };
+
+    size_t dir = start.dir;
+    const char *q;
+    for (const char *p = grid.data() + start.offset;; p = q) {
+        q = p + stride_by_direction[dir];
+        if (*q == pad_value) [[unlikely]]
             break;
 
-        while (grid(q) == '#') {
+        while (*q == '#') {
             dir = (dir + 1) & 3;
-            q = p + dxdy[dir];
+            q = p + stride_by_direction[dir];
         }
 
-        visited.push_back(State(q.x, q.y, dir));
-        unique.insert(q);
-        p = q;
-    }
-
-    return std::pair(visited, unique.size());
-}
-
-static bool walk2(MatrixView<const char> grid,
-                  Vec2u16 p,
-                  std::array<Matrix<bool>, 4> &visited,
-                  dense_set<Vec2u16> *out = nullptr)
-{
-    uint16_t dir = N;
-
-    for (Matrix<bool> &m : visited)
-        std::ranges::fill(m.all(), false);
-
-    visited[dir](p) = true;
-
-    while (true) {
-        auto q = p + dxdy[dir];
-        if (!grid.in_bounds(q))
-            break;
-
-        while (grid(q) == '#') {
-            dir = (dir + 1) & 3;
-            q = p + dxdy[dir];
-        }
-
-        if (visited[dir](q))
+        if (!visit(State(q - grid.data(), dir)))
             return false;
-        visited[dir](q) = true;
-
-        p = q;
-    }
-
-    if (out) {
-        out->clear();
-        out->reserve(visited.size());
-        for (size_t dir = 0; dir < 4; ++dir) {
-            for (auto p : visited[dir].ndindex<uint16_t>()) {
-                if (visited[dir](p))
-                    out->insert(p);
-            }
-        }
     }
 
     return true;
 }
 
+static std::vector<uint16_t> initial_walk(MatrixView<const char> grid,
+                                          const State start,
+                                          std::array<Bitset, 4> &visited)
+{
+    for (auto &v : visited)
+        v.clear();
+
+    walk(grid, start, [&](const State &s) {
+        visited[s.dir].test_and_set(s.offset);
+        return true;
+    });
+
+    Bitset combined(grid.rows * grid.cols);
+    for (size_t dir = 0; dir < 4; dir++)
+        for (size_t i = 0; uint64_t b : visited[dir].blocks)
+            combined.blocks[i++] |= b;
+
+    size_t n_unique_offsets = 0;
+    for (uint64_t b : combined.blocks)
+        n_unique_offsets += std::popcount(b);
+
+    std::vector<uint16_t> result;
+    result.reserve(n_unique_offsets);
+    combined.for_each_set_bit([&](size_t b) { result.push_back(b); });
+
+    return result;
+}
+
+static int count_loops_with_obstructions(const Matrix<char> &original_grid,
+                                         const State start,
+                                         std::span<const uint16_t> obstructions)
+{
+    // Take a copy so that each thread can modify it independently.
+    Matrix<char> grid = original_grid;
+
+    std::array<Bitset, 4> visited;
+    for (auto &v : visited)
+        v = Bitset(grid.rows * grid.cols);
+
+    int loops = 0;
+    for (uint16_t offset : obstructions) {
+        char c = grid.data()[offset];
+        if (c == '.' || c == '^') {
+            grid.data()[offset] = '#';
+            for (auto &v : visited)
+                v.clear();
+            loops += !walk(grid, start, [&](const State &s) {
+                return !visited[s.dir].test_and_set(s.offset);
+            });
+            grid.data()[offset] = c;
+        }
+    }
+
+    return loops;
+}
+
 void run(std::string_view buf)
 {
     auto lines = split_lines(buf);
-    auto grid = Matrix<char>::from_lines(lines);
+    auto grid = Matrix<char>::from_lines(lines).padded(1, pad_value);
     ASSERT(grid.rows < 256);
     ASSERT(grid.cols < 256);
 
-    Vec2u16 start{};
+    State start{};
     for (auto p : grid.ndindex<uint16_t>()) {
         if (grid(p) == '^') {
-            start = p;
+            start.offset = &grid(p) - grid.data();
             break;
         }
     }
 
-    auto [route, n_route_unique] = walk1(grid, start);
-    fmt::print("{}\n", n_route_unique);
-
-    dense_set<Vec2u16> visited_points;
-    std::array<Matrix<bool>, 4> visited_states;
+    std::array<Bitset, 4> visited_states;
     for (auto &v : visited_states)
-        v = Matrix<bool>(grid.rows, grid.cols);
-    walk2(grid, start, visited_states, &visited_points);
+        v = Bitset(grid.rows * grid.cols);
+    auto visited = initial_walk(grid, start, visited_states);
+    fmt::print("{}\n", visited.size() + 1);
 
-    int n_loops = 0;
-    for (auto p : visited_points) {
-        char c = grid(p);
-        if (c == '.' || c == '^') {
-            grid(p) = '#';
-            n_loops += !walk2(grid, start, visited_states);
-            grid(p) = c;
-        }
-    }
-    fmt::print("{}\n", n_loops);
+    std::atomic<int> total_loops = 0;
+    ThreadPool::get().for_each(visited, [&](std::span<const uint16_t> subspan) {
+        const auto loops = count_loops_with_obstructions(grid, start, subspan);
+        total_loops.fetch_add(loops);
+    });
+
+    fmt::print("{}\n", total_loops.load());
 }
 
 }
