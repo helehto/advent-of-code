67465d91ed6d367971f0855dddf63cb53094e8ea
Henrik Lehtonen <eigengrau@vm86.se>
2026-01-07 22:44:05 +0100

    2015/11: Optimize
      Year    Day    n0    n1    min t₀    min t₁     Δmin  Δmin%      avg t₀    avg t₁     Δavg  Δavg%
    ------  -----  ----  ----  --------  --------  -------  -------  --------  --------  -------  -------
      2015     11   586   879   1107.05   748.788  -358.26  -32.4%    1132.85   762.097  -370.75  -32.7%

--- 2015/11.cc
+++ 2015/11.cc
@@ -2,19 +2,19 @@
 
 namespace aoc_2015_11 {
 
-static void increment(std::span<char> s)
+static void increment(std::span<uint8_t, 8> s)
 {
     for (size_t i = s.size(); i--;) {
         s[i]++;
-        if (s[i] == 'i' || s[i] == 'o' || s[i] == 'l')
+        if (s[i] == 'i' - 'a' || s[i] == 'o' - 'a' || s[i] == 'l' - 'a')
             s[i]++;
-        if (s[i] <= 'z')
+        if (s[i] < 26)
             return;
-        s[i] = 'a';
+        s[i] = 0;
     }
 }
 
-static bool has_increasing_triple(std::string_view s)
+static bool has_increasing_triple(std::span<const uint8_t, 8> s)
 {
     return (s[0] + 1 == s[1] && s[1] + 1 == s[2]) ||
            (s[1] + 1 == s[2] && s[2] + 1 == s[3]) ||
@@ -24,13 +24,13 @@ static bool has_increasing_triple(std::string_view s)
            (s[5] + 1 == s[6] && s[6] + 1 == s[7]);
 }
 
-static bool has_nonoverlapping_pairs(std::string_view s)
+static bool has_nonoverlapping_pairs(std::span<const uint8_t, 8> s)
 {
     uint32_t mask = 0;
 
     for (size_t i = 1; i < s.size(); i++) {
         if (s[i - 1] == s[i])
-            mask |= 1U << (s[i] - 'a');
+            mask |= 1U << s[i];
     }
 
     return std::popcount(mask) >= 2;
@@ -39,13 +39,16 @@ static bool has_nonoverlapping_pairs(std::string_view s)
 void run(std::string_view buf)
 {
     ASSERT(buf.size() == 8);
-    std::string s(buf);
+    std::array<uint8_t, 8> s;
+    std::ranges::transform(buf, s.begin(), λx(x - 'a'));
 
     for (int i = 0; i < 2; i++) {
         do {
             increment(s);
-        } while (!has_increasing_triple(s) || !has_nonoverlapping_pairs(s));
-        fmt::print("{}\n", s);
+        } while (!has_nonoverlapping_pairs(s) || !has_increasing_triple(s));
+        for (uint8_t c : s)
+            fputc(c + 'a', stdout);
+        fputc('\n', stdout);
     }
 }
 
