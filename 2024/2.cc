e98829ff0bbf5a8a5580bd5a6680c63038928763
Henrik Lehtonen <eigengrau@vm86.se>
2026-01-14 00:49:54 +0100

    2024/2: Optimize
      Year    Day     n0     n1    min t₀    min t₁    Δmin  Δmin%      avg t₀    avg t₁    Δavg  Δavg%
    ------  -----  -----  -----  --------  --------  ------  -------  --------  --------  ------  -------
      2024      2  16639  40881    39.113      15.7  -23.41  -59.9%     40.018   16.2552  -23.76  -59.4%

--- 2024/2.cc
+++ 2024/2.cc
@@ -2,27 +2,79 @@
 
 namespace aoc_2024_2 {
 
-constexpr bool is_safe1(std::span<const int> nums)
+enum class Order { increasing, decreasing, invalid };
+
+constexpr bool compatible(Order order, int a, int b)
 {
-    for (size_t i = 1; i < nums.size(); ++i) {
-        int d = std::abs(nums[i] - nums[i - 1]);
-        if (d < 1 || d > 3)
-            return false;
+    DEBUG_ASSERT(order != Order::invalid);
+    int d = b - a;
+    return (order == Order::increasing && d >= 1 && d <= 3) ||
+           (order == Order::decreasing && d >= -3 && d <= -1);
+}
+
+constexpr Order get_order(std::span<const int> nums)
+{
+    const auto d = nums[1] - nums[0];
+
+    if (d >= -3 && d <= -1) {
+        for (size_t i = 1; i < nums.size(); ++i)
+            if (!compatible(Order::decreasing, nums[i - 1], nums[i]))
+                return Order::invalid;
+        return Order::decreasing;
     }
-    return std::is_sorted(nums.begin(), nums.end()) ||
-           std::is_sorted(nums.rbegin(), nums.rend());
+
+    if (d >= 1 && d <= 3) {
+        for (size_t i = 1; i < nums.size(); ++i)
+            if (!compatible(Order::increasing, nums[i - 1], nums[i]))
+                return Order::invalid;
+        return Order::increasing;
+    }
+
+    return Order::invalid;
+}
+
+constexpr Order get_concatenated_order(std::span<const int> a, std::span<const int> b)
+{
+    const Order order_a = get_order(a);
+    if (order_a == Order::invalid)
+        return Order::invalid;
+
+    const Order order_b = get_order(b);
+    if (order_b != order_a)
+        return Order::invalid; // also covers the case where b is invalid
+
+    return compatible(order_a, a.back(), b.front()) ? order_a : Order::invalid;
 }
 
 constexpr bool is_safe2(std::span<const int> nums)
 {
-    std::vector<int> v(nums.begin(), nums.end());
-    for (size_t i = nums.size(); i--;) {
-        auto saved = v[i];
-        v.erase(v.begin() + i);
-        if (is_safe1(v))
+    DEBUG_ASSERT(nums.size() >= 5);
+
+    if (get_order(nums.subspan(1)) != Order::invalid)
+        return true;
+    if (get_order(nums.subspan(0, nums.size() - 1)) != Order::invalid)
+        return true;
+
+    {
+        const auto order = get_order(nums.subspan(2));
+        if (order != Order::invalid && compatible(order, nums[0], nums[1]))
             return true;
-        v.insert(v.begin() + i, saved);
     }
+    {
+        const auto order = get_order(nums.subspan(0, nums.size() - 2));
+        if (order != Order::invalid &&
+            compatible(order, nums[nums.size() - 2], nums[nums.size() - 1]))
+            return true;
+    }
+
+    for (size_t i = 2; i + 2 < nums.size(); ++i) {
+        const auto head = std::span(nums).subspan(0, i);
+        const auto tail = std::span(nums).subspan(i + 1);
+        const auto order = get_concatenated_order(head, tail);
+        if (order != Order::invalid)
+            return true;
+    }
+
     return false;
 }
 
@@ -33,7 +85,7 @@ void run(std::string_view buf)
     small_vector<int> nums;
     for (std::string_view line : split_lines(buf)) {
         find_numbers(line, nums);
-        s1 += is_safe1(nums);
+        s1 += get_order(nums) != Order::invalid;
         s2 += is_safe2(nums);
     }
 
