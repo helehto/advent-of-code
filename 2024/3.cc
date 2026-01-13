6983e07efdd20b91db6c97aaa7acee6236f723ff
Henrik Lehtonen <eigengrau@vm86.se>
2026-01-13 23:57:56 +0100

    2024/3: Optimize by parsing manually instead of using std::regex
      Year    Day    n0     n1    min t₀    min t₁     Δmin  Δmin%      avg t₀    avg t₁     Δavg  Δavg%
    ------  -----  ----  -----  --------  --------  -------  -------  --------  --------  -------  -------
      2024      3  1328  76893   479.666     8.576  -471.09  -98.2%    501.943   8.68412  -493.26  -98.3%

--- 2024/3.cc
+++ 2024/3.cc
@@ -3,54 +3,88 @@
 
 namespace aoc_2024_3 {
 
-static int match_product(std::string_view s, const std::match_results<const char *> &m)
+constexpr int parse_mul(const char *p)
 {
-    std::string_view a_match(s.data() + m.position(1), m.length(1));
-    std::string_view b_match(s.data() + m.position(2), m.length(2));
+    DEBUG_ASSERT(*p == ')');
+
+    const char *a1 = p--;
+    if (!(*p >= '0' && *p <= '9'))
+        return 0;
+
+    do {
+        --p;
+    } while (*p >= '0' && *p <= '9');
+    const char *a0 = p + 1;
+
+    if (*p != ',')
+        return 0;
+
+    const char *b1 = p--;
+    if (!(*p >= '0' && *p <= '9'))
+        return 0;
+
+    do {
+        --p;
+    } while (*p >= '0' && *p <= '9');
+    const char *b0 = p + 1;
+
+    if (*--p != 'l')
+        return 0;
+    if (*--p != 'u')
+        return 0;
+    if (*--p != 'm')
+        return 0;
 
     int a = 0;
+    for (const char *q = a0; q < a1; ++q)
+        a = a * 10 + (*q - '0');
+
     int b = 0;
-    std::from_chars(a_match.data(), a_match.data() + a_match.size(), a);
-    std::from_chars(b_match.data(), b_match.data() + b_match.size(), b);
+    for (const char *q = b0; q < b1; ++q)
+        b = b * 10 + (*q - '0');
+
     return a * b;
 }
 
-static int part1(std::string_view s)
+constexpr bool is_do(const char *p)
 {
-    std::regex re(R"(mul\((\d+),(\d+)\))");
-    auto it = std::cregex_iterator(s.begin(), s.end(), re);
-    auto end = std::cregex_iterator();
-
-    int result = 0;
-    for (; it != end; ++it)
-        result += match_product(s, *it);
-    return result;
+    DEBUG_ASSERT(*p == ')');
+    return p[-3] == 'd' && p[-2] == 'o' && p[-1] == '(';
 }
 
-static int part2(std::string_view s)
+constexpr bool is_dont(const char *p)
 {
-    std::regex re(R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
-    auto it = std::cregex_iterator(s.begin(), s.end(), re);
-    auto end = std::cregex_iterator();
-
-    int result = 0;
-    bool enabled = true;
-    for (; it != end; ++it) {
-        std::string_view text(s.data() + it->position(0), it->length(0));
-        if (text.starts_with("don't"))
-            enabled = false;
-        else if (text.starts_with("do"))
-            enabled = true;
-        else if (enabled)
-            result += match_product(s, *it);
-    }
-    return result;
+    DEBUG_ASSERT(*p == ')');
+    return p[-6] == 'd' && p[-5] == 'o' && p[-4] == 'n' && p[-3] == '\'' &&
+           p[-2] == 't' && p[-1] == '(';
 }
 
 void run(std::string_view buf)
 {
-    fmt::print("{}\n", part1(buf));
-    fmt::print("{}\n", part2(buf));
+    int part1 = 0;
+    int part2 = 0;
+    int mask = -1;
+
+    size_t i = SIZE_MAX;
+    while (true) {
+        i = buf.find(')', i + 1);
+        if (i == std::string_view::npos)
+            break;
+
+        const char *p = &buf[i];
+        if (is_do(p)) {
+            mask = -1;
+        } else if (is_dont(p)) {
+            mask = 0;
+        } else {
+            int value = parse_mul(p);
+            part1 += value;
+            part2 += value & mask;
+        }
+    }
+
+    fmt::print("{}\n", part1);
+    fmt::print("{}\n", part2);
 }
 
 }
