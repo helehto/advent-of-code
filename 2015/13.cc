77a3e5592b8a0f404d5c0620630030bc69ba1c27
Henrik Lehtonen <eigengrau@vm86.se>
2026-01-14 19:07:18 +0100

    2015/13: Optimize via multithreading
      Year    Day    n0    n1    min t₀    min t₁     Δmin  Δmin%      avg t₀    avg t₁      Δavg  Δavg%
    ------  -----  ----  ----  --------  --------  -------  -------  --------  --------  --------  -------
      2015     13   311  2322   2135.25   221.948  -1913.3  -89.6%     2168.2   285.235  -1882.96  -86.8%

--- 2015/13.cc
+++ 2015/13.cc
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "dense_map.h"
+#include "thread_pool.h"
 
 namespace aoc_2015_13 {
 
@@ -51,24 +52,82 @@ static int total_happiness(MatrixView<const int> matrix, std::span<int> perm)
     return result;
 }
 
-static int max_happiness(MatrixView<const int> matrix)
+constexpr int64_t factorials[] = {
+    UINT64_C(1),
+    UINT64_C(1),
+    UINT64_C(2),
+    UINT64_C(6),
+    UINT64_C(24),
+    UINT64_C(120),
+    UINT64_C(720),
+    UINT64_C(5040),
+    UINT64_C(40320),
+    UINT64_C(362880),
+    UINT64_C(3628800),
+    UINT64_C(39916800),
+    UINT64_C(479001600),
+    UINT64_C(6227020800),
+    UINT64_C(87178291200),
+    UINT64_C(1307674368000),
+    UINT64_C(20922789888000),
+    UINT64_C(355687428096000),
+    UINT64_C(6402373705728000),
+    UINT64_C(121645100408832000),
+    UINT64_C(2432902008176640000),
+};
+
+/// Returns the nth permutation of [0, 1, 2, ..., n-1].
+static std::vector<int> nth_permutation(size_t n, size_t index)
 {
-    std::vector<int> perm(matrix.rows);
+    // cf. <https://en.wikipedia.org/wiki/Lehmer_code#Encoding_and_decoding>
+
+    std::vector<int> indices(n);
+    for (size_t i = indices.size(); i--;) {
+        indices[indices.size() - 1 - i] = index / factorials[i];
+        index = index % factorials[i];
+    }
+
+    std::vector<int> perm(n);
     for (size_t i = 0; i < perm.size(); i++)
         perm[i] = i;
 
-    int max_happiness = 0;
-    do {
-        int total = total_happiness(matrix, perm);
-        max_happiness = std::max(max_happiness, total);
-    } while (std::next_permutation(begin(perm), end(perm)));
+    std::vector<int> result;
+    for (size_t i = 0; i < indices.size(); i++) {
+        result.push_back(perm[indices[i]]);
+        perm.erase(perm.begin() + indices[i]);
+    }
 
-    return max_happiness;
+    return result;
+}
+
+static int max_happiness(MatrixView<const int> matrix)
+{
+    std::atomic<int> result = 0;
+
+    auto max_happiness_between = [&](size_t start, size_t end) {
+        auto perm = nth_permutation(matrix.rows, start);
+        int happiness = 0;
+        for (size_t i = start; i < end; i++) {
+            int total = total_happiness(matrix, perm);
+            happiness = std::max(happiness, total);
+            std::ranges::next_permutation(perm);
+        }
+        return happiness;
+    };
+
+    const auto n = factorials[matrix.rows];
+    ThreadPool::get().for_each_index(0, n, [&](size_t start, size_t end) {
+        atomic_store_max(result, max_happiness_between(start, end));
+    });
+
+    return result.load();
 }
 
 void run(std::string_view buf)
 {
     auto matrix = get_happiness_matrix(buf);
+    ASSERT(matrix.rows + 1 < std::size(factorials));
+
     fmt::print("{}\n", max_happiness(matrix));
 
     Matrix<int> augmented(matrix.rows + 1, matrix.cols + 1);
