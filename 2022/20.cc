011ae012ea55ff332d48508ad091eacf11c89ef2
Henrik Lehtonen <eigengrau@vm86.se>
2026-02-19 19:35:57 +0100

    2022/20: Use std::find()
          Year    Day    min t₀    min t₁      Δmin  Δmin%      avg t₀    avg t₁    Δavg  Δavg%
    --  ------  -----  --------  --------  --------  -------  --------  --------  ------  -------
          2022     20   16777.6     12709  -4068.61  -24.3%    17135.8   12837.2    -4.3  -25.1%

--- 2022/20.cc
+++ 2022/20.cc
@@ -1,65 +1,8 @@
 #include "common.h"
-#include <algorithm>
-#include <cinttypes>
-#include <cstring>
-#include <x86intrin.h>
 
 namespace aoc_2022_20 {
 
 constexpr int index_shift = 16;
-
-static size_t find_avx(const int64_t *p, const size_t n, const int64_t needle)
-{
-    const auto vneedle = _mm256_set1_epi64x(needle);
-    size_t k = 0;
-
-    // 4-way unrolled loop, comparing 16 elements at time.
-    for (; k + 15 < n; k += 16) {
-        const auto v0 = _mm256_loadu_si256((const __m256i *)&p[k + 0]);
-        const auto v1 = _mm256_loadu_si256((const __m256i *)&p[k + 4]);
-        const auto v2 = _mm256_loadu_si256((const __m256i *)&p[k + 8]);
-        const auto v3 = _mm256_loadu_si256((const __m256i *)&p[k + 12]);
-
-        const auto eq0 = _mm256_cmpeq_epi64(v0, vneedle);
-        const auto eq1 = _mm256_cmpeq_epi64(v1, vneedle);
-        const auto eq2 = _mm256_cmpeq_epi64(v2, vneedle);
-        const auto eq3 = _mm256_cmpeq_epi64(v3, vneedle);
-
-        const auto eq0ps = _mm256_castsi256_ps(eq0);
-        const auto eq1ps = _mm256_castsi256_ps(eq1);
-        const auto eq2ps = _mm256_castsi256_ps(eq2);
-        const auto eq3ps = _mm256_castsi256_ps(eq3);
-
-        const uint8_t masks01_32[] = {
-            static_cast<uint8_t>(_mm256_movemask_ps(eq0ps)),
-            static_cast<uint8_t>(_mm256_movemask_ps(eq1ps)),
-            static_cast<uint8_t>(_mm256_movemask_ps(eq2ps)),
-            static_cast<uint8_t>(_mm256_movemask_ps(eq3ps)),
-        };
-
-        uint32_t masks01;
-        memcpy(&masks01, masks01_32, sizeof(masks01));
-
-        if (masks01)
-            return k + std::countr_zero(masks01) / 2;
-    }
-
-    // Vectorized loop for 4-15 remaining elements.
-    for (; k + 4 < n; k += 4) {
-        const auto v = _mm256_loadu_si256((const __m256i *)&p[k]);
-        const auto eq = _mm256_cmpeq_epi64(v, vneedle);
-
-        if (const uint32_t mask = _mm256_movemask_epi8(eq))
-            return k + std::countr_zero(mask) / 8;
-    }
-
-    // Scalar loop for 1-3 remaining elements.
-    for (; k < n; k++)
-        if (p[k] == needle)
-            return k;
-
-    std::unreachable();
-}
 
 static void mix(const std::vector<int64_t> &original, std::vector<int64_t> &nums)
 {
@@ -68,7 +11,7 @@ static void mix(const std::vector<int64_t> &original, std::vector<int64_t> &nums
 
     // Who needs linked lists when you have an L1 cache and a prefetcher?
     for (const auto x : original) {
-        const size_t i = find_avx(data, size, x);
+        const size_t i = std::find(data, data + size, x) - data;
         const size_t j = modulo<int64_t>(i + (x >> index_shift), nums.size() - 1);
 
         if (i < j)
@@ -89,7 +32,7 @@ static int64_t solve(std::vector<int64_t> nums, int key, int rounds, int64_t zer
     for (int i = 0; i < rounds; i++)
         mix(original, nums);
 
-    const auto zero_index = find_avx(nums.data(), nums.size(), zero);
+    const auto zero_index = std::ranges::find(nums, zero) - nums.begin();
     return (nums[(zero_index + 1000) % nums.size()] >> index_shift) +
            (nums[(zero_index + 2000) % nums.size()] >> index_shift) +
            (nums[(zero_index + 3000) % nums.size()] >> index_shift);
