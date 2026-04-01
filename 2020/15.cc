965a401be5658d28be6c7087ebc489a44765fa2b
Henrik Lehtonen <eigengrau@vm86.se>
2026-04-01 23:20:27 +0200

    2020/15: Optimize
          Year    Day    min t₀    min t₁      Δmin  Δmin%      avg t₀    avg t₁    Δavg  Δavg%
    --  ------  -----  --------  --------  --------  -------  --------  --------  ------  -------
          2020     15    130225    107267  -22958.2  -17.6%     138360    110196  -28.16  -20.4%

--- 2020/15.cc
+++ 2020/15.cc
@@ -1,48 +1,87 @@
 #include "common.h"
-#include "dense_map.h"
 
 namespace aoc_2020_15 {
 
-/// The cut-off index after which value are cached in a dense_map instead.
-constexpr int32_t sparse_cutoff = 2'000'000;
-
-static int32_t step(int32_t turn,
-                    int32_t last,
-                    std::vector<int32_t> &seen_dense,
-                    dense_map<int32_t, int32_t> &seen_sparse)
-{
-    if (last < sparse_cutoff) {
-        const int32_t t = std::exchange(seen_dense[last], turn - 1);
-        return t >= 0 ? turn - t - 1 : 0;
-    } else {
-        if (auto [it, inserted] = seen_sparse.emplace(last, turn - 1); !inserted) {
-            const int32_t t = std::exchange(it->second, turn - 1);
-            return turn - t - 1;
-        }
-        return 0;
-    }
-}
-
 void run(std::string_view buf)
 {
-    small_vector<int32_t> init;
+    small_vector<uint32_t> init;
     find_numbers(buf, init);
 
-    std::vector<int32_t> seen_dense(sparse_cutoff, -1);
-    for (size_t i = 0; i < init.size() - 1; i++)
-        seen_dense[init[i]] = i + 1;
+    std::vector<uint32_t> prev(30'000'000);
 
-    dense_map<int32_t, int32_t> seen_sparse;
-    seen_sparse.reserve(1'000'000);
+    // `seen_bitset` tracks which numbers that have been seen before (i.e.
+    // which indices in `prev` are valid).
+    //
+    // Why not use a sentinel value for this? `prev` is 120MB, far too large to
+    // fit even in L3. This, combined with the random access pattern, means the
+    // majority of steps to compute the next number involves an expensive load
+    // from main memory.
+    //
+    // As the bottleneck is a loop-carried dependency which involves a lookup
+    // in `prev`, storing it there would cause a long stall even for numbers
+    // that have not been seen before. The next number is 0 by definition, but
+    // we would still have to wait for the load to complete to know that.
+    //
+    // Compressing this information into a bitset and storing it separately
+    // instead makes it ~3.5 MB. This fits partially in L2 and fully in L3, and
+    // means that we can identify whether we are looking at a completely new
+    // number, check if the next number is 0 and break the dependency chain if
+    // so with only the latency of an L3 hit at worst.
+    //
+    // Even if we have seen the number before and _do_ need to load from `prev`
+    // to compute the next number, that load can be issued speculatively and
+    // run in parallel if the branch is correctly predicted, so we will likely
+    // not be worse off anyway.
+    std::vector<uint64_t> seen_bitset((prev.size() + 63) / 64, 0);
 
-    int32_t last = init.back();
-    int32_t turn = init.size() + 1;
-    for (; turn <= 2020; turn++)
-        last = step(turn, last, seen_dense, seen_sparse);
+    // 0 is the most common individual value in the sequence (~10%). Track it
+    // in a separate variable here, and handle it in a separate branch in
+    // step() below, to coax the compiler into keeping it in a register, which
+    // avoids a bunch of memory ops.
+    uint32_t zero = 0;
+
+    for (size_t i = 0; i < init.size(); i++) {
+        if (init[i] == 0) {
+            zero = i + 1;
+            seen_bitset[zero / 64] |= UINT64_C(1) << (zero % 64);
+        } else {
+            prev[init[i]] = i + 1;
+            seen_bitset[init[i] / 64] |= UINT64_C(1) << (init[i] % 64);
+        }
+    }
+
+    auto test_and_mark_as_seen = [&](size_t n) {
+        const size_t index = n / 64;
+        const uint64_t mask = UINT64_C(1) << (n % 64);
+        if (seen_bitset[index] & mask)
+            return true;
+        seen_bitset[index] |= mask;
+        return false;
+    };
+
+    auto step = [&](uint32_t turn, uint32_t last) -> uint32_t {
+        if (last == 0) {
+            const uint32_t delta = turn - zero;
+            zero = turn;
+            return delta;
+        } else if (!test_and_mark_as_seen(last)) {
+            prev[last] = turn;
+            return 0;
+        } else {
+            const uint32_t delta = turn - prev[last];
+            prev[last] = turn;
+            return delta;
+        }
+    };
+
+    uint32_t last = init.back();
+    uint32_t turn = init.size();
+    for (; turn < 2020; turn++)
+        last = step(turn, last);
     fmt::print("{}\n", last);
 
-    for (; turn <= 30'000'000; turn++)
-        last = step(turn, last, seen_dense, seen_sparse);
+    for (; turn < 30'000'000; turn++)
+        last = step(turn, last);
     fmt::print("{}\n", last);
 }
 
