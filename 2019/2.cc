b2ea01229c5e0f280973aceba1bdb6f20795d325
Henrik Lehtonen <eigengrau@vm86.se>
2026-08-13 15:52:16 +0200

    2019/2: Optimize by replacing brute-force with algebra
          Year    Day    min t₀    min t₁     Δmin  Δmin%      avg t₀    avg t₁    Δavg  Δavg%
    --  ------  -----  --------  --------  -------  -------  --------  --------  ------  -------
          2019      2   835.901      0.46  -835.44  -99.9%    892.659     0.495   -0.89  -99.9%

--- 2019/2.cc
+++ 2019/2.cc
@@ -1,32 +1,44 @@
 #include "common.h"
-#include "intcode.h"
 
 namespace aoc_2019_2 {
 
-using VM = IntcodeVM<FlatMemory<int>>;
-
 void run(std::string_view buf)
 {
-    auto prog = find_numbers<VM::value_type>(buf);
+    small_vector<uint32_t, 256> prog;
+    find_numbers(buf, prog);
 
-    VM vm;
-    vm.reset(prog);
-    vm.mem.wr(1, 12);
-    vm.mem.wr(2, 5);
-    vm.run();
-    fmt::print("{}\n", vm.mem.rd(0));
-
-    for (int verb = 1; verb < 100; ++verb) {
-        for (int noun = 1; noun < 100; ++noun) {
-            vm.reset(prog);
-            vm.mem.wr(1, noun);
-            vm.mem.wr(2, verb);
-            vm.run();
-            if (vm.mem.rd(0) == 19690720) {
-                fmt::print("{}\n", 100 * noun + verb);
-                break;
-            }
+    auto run_program = [&](uint32_t noun, uint32_t verb) {
+        auto mem = prog;
+        mem[1] = noun;
+        mem[2] = verb;
+        for (int pc = 0;; pc += 4) {
+            if (mem[pc] == 1)
+                mem[mem[pc + 3]] = mem[mem[pc + 1]] + mem[mem[pc + 2]];
+            else if (mem[pc] == 2)
+                mem[mem[pc + 3]] = mem[mem[pc + 1]] * mem[mem[pc + 2]];
+            else if (mem[pc] == 99)
+                return mem[0];
+            else
+                ASSERT_MSG(false, "Invalid opcode {} at pc={}", mem[pc], pc);
         }
-    }
+    };
+
+    // The intcode program computes `noun * c₁ + verb + c₂` for some constants
+    // c₁ and c₂ that depend on the specific input program.
+    //
+    // Running with noun=0, verb=0 yields c₂.
+    // Running with noun=1, verb=0 yields c₁ + c₂, from which we get c₁.
+    auto c2 = run_program(0, 0);
+    auto c1 = run_program(1, 0) - c2;
+    fmt::print("{}\n", 12 * c1 + 2 + c2);
+
+    // Given c₁ ≥ 100, 0 ≤ noun < 100 and 0 ≤ verb < 100, the only solution to
+    // `noun * c₁ + verb + c₂ = 19690720` is this:
+    const auto noun = (19690720 - c2) / c1;
+    const auto verb = (19690720 - c2) % c1;
+    ASSERT(noun < 100);
+    ASSERT(verb < 100);
+    fmt::print("{}\n", 100 * noun + verb);
 }
+
 }
