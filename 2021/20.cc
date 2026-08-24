76c4e437c84ce162762f7c1c44424018480dcdc2
Henrik Lehtonen <eigengrau@vm86.se>
2026-08-24 21:28:13 +0200

    2021/20: Optimize via SIMD lookup table for 3x3 kernel
    The first optimization which introduced SIMD was severely bottlenecked
    by a scalar gather loop mapping the 3x3 neighborhood to an output pixel
    via the lookup table.
    
    Implement a SIMD lookup instead, mapping 16/32/64 input pixels to output
    pixels at a time. To be compatible with 128-bit vectors (e.g. NEON), two
    similar but slightly different techniques are needed, since the vector
    width determines whether we can fit the table in 2x256-bit vectors, or
    need to split it into 4x128-bit vectors. The latter requires a two-table
    lookup per half.
    
    Also unroll the y loop twice to be able to share loaded input rows; this
    turns a 1:9 output:input ratio into 2:12, reducing loads by 1/3. This
    yields an additional ~10% speedup on top of the SIMD-ized table lookup.
    
          Year    Day    min t₀    min t₁    Δmin  Δmin%      avg t₀    avg t₁     Δavg  Δavg%
    --  ------  -----  --------  --------  ------  -------  --------  --------  -------  -------
          2021     20   485.399      53.3  -432.1  -89.0%    500.424    56.063  -444.36  -88.8%

--- 2021/20.cc
+++ 2021/20.cc
@@ -16,12 +16,104 @@ namespace aoc_2021_20 {
 
 namespace hn = hwy::HWY_NAMESPACE;
 
+using D = hn::ScalableTag<uint8_t>;
+constexpr D d;
+
+// 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, ... repeating.
+HWY_ALIGN_MAX constexpr auto bitmask_lookup_array =
+    std::to_array<uint8_t>({0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x01, 0x02,
+                            0x04, 0x08, 0x10, 0x20, 0x40, 0x80});
+static const hn::Vec<D> bitmask_lookup = hn::LoadDup128(d, bitmask_lookup_array.data());
+
+// Variant for 128-bit SIMD. The table is split across 4 vectors, each pair
+// representing one half of the table.
+class Table128x4 {
+    hn::Vec<D> pattern0_lo; // bits 0-127
+    hn::Vec<D> pattern0_hi; // bits 128-255
+    hn::Vec<D> pattern1_lo; // bits 256-383
+    hn::Vec<D> pattern1_hi; // bits 384-511
+
+    hn::Vec<D>
+    lookup(const hn::Vec<D> lo, const hn::Vec<D> hi, const hn::Vec<D> index) const
+    {
+        const hn::Vec<D> byte_idx = hn::ShiftRight<3>(index);
+        const hn::Vec<D> bitsets =
+            hn::TwoTablesLookupLanes(d, lo, hi, hn::IndicesFromVec(d, byte_idx));
+        const hn::Vec<D> bit_idx = hn::And(index, hn::Set(d, 7));
+        const hn::Vec<D> bitmask = hn::TableLookupBytes(bitmask_lookup, bit_idx);
+        return hn::VecFromMask(hn::TestBit(bitsets, bitmask));
+    }
+
+public:
+    Table128x4(std::span<const uint8_t, 512 / 8> bits)
+        : pattern0_lo(hn::LoadU(d, &bits[0]))
+        , pattern0_hi(hn::LoadU(d, &bits[16]))
+        , pattern1_lo(hn::LoadU(d, &bits[32]))
+        , pattern1_hi(hn::LoadU(d, &bits[48]))
+    {
+    }
+
+    hn::Vec<D> map(const hn::Vec<D> selector, const hn::Vec<D> index) const
+    {
+        const hn::Vec<D> result0 = lookup(pattern0_lo, pattern0_hi, index);
+        const hn::Vec<D> result1 = lookup(pattern1_lo, pattern1_hi, index);
+        return hn::IfVecThenElse(selector, result1, result0);
+    }
+};
+
+// Variant for ≥256-bit SIMD. Each table half fits neatly in a single vector.
+//
+// Note that >256-bit vectors don't help much here, other than mapping more
+// elements at a time. We are limited by the 8-bit lanes rather than the vector
+// width; since the 3x3 neighborhood defines a 512-bit table that requires 9
+// bits to index, we need to split the table into two halves anyway and blend
+// the results.
+class Table256x2 {
+    hn::Vec<D> pattern0;
+    hn::Vec<D> pattern1;
+
+    hn::Vec<D> lookup(const hn::Vec<D> table, const hn::Vec<D> index) const
+    {
+        const hn::Vec<D> byte_idx = hn::ShiftRight<3>(index);
+        const hn::Vec<D> bitsets =
+            hn::TableLookupLanes(table, hn::IndicesFromVec(d, byte_idx));
+        const hn::Vec<D> bit_idx = hn::And(index, hn::Set(d, 7));
+        const hn::Vec<D> bitmask = hn::TableLookupBytes(bitmask_lookup, bit_idx);
+        return hn::VecFromMask(hn::TestBit(bitsets, bitmask));
+    }
+
+public:
+    Table256x2(std::span<const uint8_t, 512 / 8> bits)
+        : pattern0(hn::LoadN(d, &bits[0], 32))
+        , pattern1(hn::LoadN(d, &bits[32], 32))
+    {
+    }
+
+    hn::Vec<D> map(const hn::Vec<D> selector, const hn::Vec<D> index) const
+    {
+        const hn::Vec<D> result0 = lookup(pattern0, index);
+        const hn::Vec<D> result1 = lookup(pattern1, index);
+        return hn::IfVecThenElse(selector, result1, result0);
+    }
+};
+
+static_assert(hn::MaxLanes(d) >= 16);
+using LookupTable = std::conditional_t<hn::MaxLanes(d) == 16, Table128x4, Table256x2>;
+
+static std::array<uint8_t, 512 / 8> parse_pattern(std::string_view pat)
+{
+    ASSERT(pat.size() == 512);
+    std::array<uint8_t, 512 / 8> pattern_bits{};
+    for (size_t i = 0; i < 512; ++i)
+        pattern_bits[i / 8] |= (pat[i] == '#') ? (1 << (i % 8)) : 0;
+    return pattern_bits;
+}
+
 void run(std::string_view buf, aoc::Answer &answer)
 {
-    HWY_ALIGN_MAX std::array<int8_t, 512> pattern;
+    const bool first_is_lit = buf[0] == '#';
     auto lines = split_lines(buf);
-    for (size_t i = 0; buf[i] != '\n'; ++i)
-        pattern[i] = (buf[i] == '#') ? -1 : 0;
+    LookupTable table(parse_pattern(lines[0]));
 
     auto image_lines = std::span(lines).subspan(2);
     constexpr int max_iterations = 50;
@@ -41,78 +133,66 @@ void run(std::string_view buf, aoc::Answer &answer)
     size_t y0 = max_iterations + 1;
     size_t y1 = y0 + init_rows - 1;
 
-    auto storage = hwy::MakeUniqueAlignedArray<int8_t>(2 * rows * cols);
-    MatrixView<int8_t> image(storage.get(), rows, cols);
-    MatrixView<int8_t> new_image(storage.get() + rows * cols, rows, cols);
+    auto storage = hwy::MakeUniqueAlignedArray<uint8_t>(2 * rows * cols);
+    MatrixView<uint8_t> image(storage.get(), rows, cols);
+    MatrixView<uint8_t> new_image(storage.get() + rows * cols, rows, cols);
 
     for (size_t y = y0; y <= y1; ++y)
         for (size_t x = x0; x <= x1; ++x)
-            image(y, x) = (image_lines[y - y0][x - x0] == '#') ? -1 : 0;
+            image(y, x) = (image_lines[y - y0][x - x0] == '#') ? 0xff : 0;
 
-    using D = hn::ScalableTag<int8_t>;
-    constexpr D d;
-    using D16 = hn::ScalableTag<int16_t>;
-    constexpr D16 d16;
+    const hn::Vec<D> k7 = hn::Set(d, 1 << 7);
+    const hn::Vec<D> k6 = hn::Set(d, 1 << 6);
+    const hn::Vec<D> k5 = hn::Set(d, 1 << 5);
+    const hn::Vec<D> k4 = hn::Set(d, 1 << 4);
+    const hn::Vec<D> k3 = hn::Set(d, 1 << 3);
+    const hn::Vec<D> k2 = hn::Set(d, 1 << 2);
+    const hn::Vec<D> k1 = hn::Set(d, 1 << 1);
+    const hn::Vec<D> k0 = hn::Set(d, 1 << 0);
+
+    auto make_index = [&](const hn::Vec<D> uc, const hn::Vec<D> ur, const hn::Vec<D> ml,
+                          const hn::Vec<D> mc, const hn::Vec<D> mr, const hn::Vec<D> ll,
+                          const hn::Vec<D> lc, const hn::Vec<D> lr) {
+        const hn::Vec<D> u = hn::Xor(uc & k7, ur & k6);
+        const hn::Vec<D> m = hn::Xor3(ml & k5, mc & k4, mr & k3);
+        const hn::Vec<D> l = hn::Xor3(ll & k2, lc & k1, lr & k0);
+        return hn::Xor3(u, m, l);
+    };
 
     bool outside_is_lit = false;
     for (size_t n = 0; n < max_iterations; ++n, outside_is_lit = !outside_is_lit) {
-        const int8_t pad = pattern[0] && !outside_is_lit ? -1 : 0;
+        const uint8_t pad = first_is_lit && !outside_is_lit ? 0xff : 0;
 
-        // Fill in the interior of the new image.
-        for (size_t y = y0 - 1; y <= y1 + 1; ++y) {
+        // Fill in the interior of the new image. Unrolled twice along y to
+        // enable output rows to share loaded input rows.
+        for (size_t y = y0 - 1; y <= y1 + 1; y += 2) {
             // NB: the matrix is sized so that we can deliberately over-read
             // past the current bounding box, meaning that we don't need a
             // scalar loop to handle the tail.
-            for (size_t x = x0 - 1; x <= x1 + 1; x += hn::Lanes(d16)) {
+            for (size_t x = x0 - 1; x <= x1 + 1; x += hn::Lanes(d)) {
                 const auto *HWY_RESTRICT p = &image(y, x);
-                const hn::Vec<D> ul8 = hn::LoadU(d, p - cols - 1);
-                const hn::Vec<D> uc8 = hn::LoadU(d, p - cols + 0);
-                const hn::Vec<D> ur8 = hn::LoadU(d, p - cols + 1);
-                const hn::Vec<D> cl8 = hn::LoadU(d, p - 1);
-                const hn::Vec<D> cc8 = hn::LoadU(d, p + 0);
-                const hn::Vec<D> cr8 = hn::LoadU(d, p + 1);
-                const hn::Vec<D> ll8 = hn::LoadU(d, p + cols - 1);
-                const hn::Vec<D> lc8 = hn::LoadU(d, p + cols + 0);
-                const hn::Vec<D> lr8 = hn::LoadU(d, p + cols + 1);
+                const hn::Vec<D> l0 = hn::LoadU(d, p - 1 * cols - 1);
+                const hn::Vec<D> c0 = hn::LoadU(d, p - 1 * cols + 0);
+                const hn::Vec<D> r0 = hn::LoadU(d, p - 1 * cols + 1);
+                const hn::Vec<D> l1 = hn::LoadU(d, p + 0 * cols - 1);
+                const hn::Vec<D> c1 = hn::LoadU(d, p + 0 * cols + 0);
+                const hn::Vec<D> r1 = hn::LoadU(d, p + 0 * cols + 1);
+                const hn::Vec<D> l2 = hn::LoadU(d, p + 1 * cols - 1);
+                const hn::Vec<D> c2 = hn::LoadU(d, p + 1 * cols + 0);
+                const hn::Vec<D> r2 = hn::LoadU(d, p + 1 * cols + 1);
+                const hn::Vec<D> l3 = hn::LoadU(d, p + 2 * cols - 1);
+                const hn::Vec<D> c3 = hn::LoadU(d, p + 2 * cols + 0);
+                const hn::Vec<D> r3 = hn::LoadU(d, p + 2 * cols + 1);
 
-                // Since the table is 512 bits we need 9 bits to encode the
-                // index; sign-extend to 16 bits to handle bit 8 (upper left
-                // pixel of the 3x3 neighborhood).
-                const hn::Vec<D16> ul16 = hn::PromoteLowerTo(d16, ul8);
-                const hn::Vec<D16> uc16 = hn::PromoteLowerTo(d16, uc8);
-                const hn::Vec<D16> ur16 = hn::PromoteLowerTo(d16, ur8);
-                const hn::Vec<D16> cl16 = hn::PromoteLowerTo(d16, cl8);
-                const hn::Vec<D16> cc16 = hn::PromoteLowerTo(d16, cc8);
-                const hn::Vec<D16> cr16 = hn::PromoteLowerTo(d16, cr8);
-                const hn::Vec<D16> ll16 = hn::PromoteLowerTo(d16, ll8);
-                const hn::Vec<D16> lc16 = hn::PromoteLowerTo(d16, lc8);
-                const hn::Vec<D16> lr16 = hn::PromoteLowerTo(d16, lr8);
+                // The top-left bit (l0/11) selects which table half to use.
+                // The rest are treated as a bit index into that.
+                const hn::Vec<D> index0 = make_index(c0, r0, l1, c1, r1, l2, c2, r2);
+                const hn::Vec<D> index1 = make_index(c1, r1, l2, c2, r2, l3, c3, r3);
 
-                const hn::Vec<D16> index = (ul16 & hn::Set(d16, UINT16_C(1) << 8)) |
-                                           (uc16 & hn::Set(d16, UINT16_C(1) << 7)) |
-                                           (ur16 & hn::Set(d16, UINT16_C(1) << 6)) |
-                                           (cl16 & hn::Set(d16, UINT16_C(1) << 5)) |
-                                           (cc16 & hn::Set(d16, UINT16_C(1) << 4)) |
-                                           (cr16 & hn::Set(d16, UINT16_C(1) << 3)) |
-                                           (ll16 & hn::Set(d16, UINT16_C(1) << 2)) |
-                                           (lc16 & hn::Set(d16, UINT16_C(1) << 1)) |
-                                           (lr16 & hn::Set(d16, UINT16_C(1) << 0));
-
-                using DU16 = hn::RebindToUnsigned<D16>;
-                HWY_ALIGN_MAX std::array<uint16_t, hn::MaxLanes(DU16())> indices;
-                hn::Store(hn::BitCast(DU16(), index), DU16(), indices.data());
-
-                int8_t *HWY_RESTRICT out = &new_image(y, x);
-                for (size_t i = 0; i < hn::Lanes(d16); i += 8) {
-                    out[i + 0] = pattern[indices[i + 0]];
-                    out[i + 1] = pattern[indices[i + 1]];
-                    out[i + 2] = pattern[indices[i + 2]];
-                    out[i + 3] = pattern[indices[i + 3]];
-                    out[i + 4] = pattern[indices[i + 4]];
-                    out[i + 5] = pattern[indices[i + 5]];
-                    out[i + 6] = pattern[indices[i + 6]];
-                    out[i + 7] = pattern[indices[i + 7]];
-                }
+                const hn::Vec<D> result0 = table.map(l0, index0);
+                const hn::Vec<D> result1 = table.map(l1, index1);
+                hn::StoreU(result0, d, &new_image(y, x));
+                hn::StoreU(result1, d, &new_image(y + 1, x));
             }
         }
 
@@ -144,13 +224,12 @@ void run(std::string_view buf, aoc::Answer &answer)
             // Be careful only to count inside the bounding box: the SIMD loop
             // writes stray pixels past its end that we shouldn't count.
             for (size_t y = y0; y <= y1; ++y) {
-                const int8_t *HWY_RESTRICT p = &image(y, x0);
-                count += std::count(p, p + x1 - x0 + 1, -1);
+                const uint8_t *HWY_RESTRICT p = &image(y, x0);
+                count += std::count(p, p + x1 - x0 + 1, 0xff);
             }
             answer.add(count);
         }
     }
 }
 AOC_REGISTER_SOLVER(2021, 20, run);
-
 }
