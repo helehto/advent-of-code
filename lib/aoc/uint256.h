e9805293f4cf003ca20f2fe259ffcabdd5835181
Henrik Lehtonen <eigengrau@vm86.se>
2026-09-03 23:13:18 +0200

    uint256: Add support for 128-bit vectors
    Obviously not as efficent, but at least makes it portable to e.g. NEON.

--- lib/aoc/uint256.h
+++ lib/aoc/uint256.h
@@ -13,29 +13,43 @@ namespace hn = hwy::HWY_NAMESPACE;
 
 /// A 256-bit unsigned integer, with a subset of the usual operations.
 struct uint256 {
-    using D = hn::FixedTag<uint64_t, 4>;
+    using D = hn::CappedTag<uint64_t, 4>;
+    static constexpr D d{};
+    static_assert(hn::MaxLanes(d) == 2 || hn::MaxLanes(d) == 4);
 
     // This array holds the 64-bit limbs, stored as little-endian (i.e.
     // limbs[0] is least significant).
     //
     // Some operations, e.g. bitwise ops and shifts, load and operate on the
-    // limbs as a single 256-bit vector. This helps the compiler to fuse
-    // operations, e.g. ~a&b into a vpandn instruction, or any 3-operand
-    // bitwise function into a single vpternlog instruction if AVX-512 is
-    // available.
+    // limbs as a single 256-bit vector when possible. This helps the compiler
+    // to fuse operations, e.g. ~a&b into a vpandn instruction, or any
+    // 3-operand bitwise function into a single vpternlog instruction if
+    // AVX-512 is available.
+    //
+    // For targets without 256-bit vector support, the limbs are operated on as
+    // two 128-bit vectors.
     alignas(32) std::array<uint64_t, 4> limbs;
 
     uint256() noexcept = default;
 
-    uint256(hn::Vec<D> v) noexcept { hn::Store(v, D(), limbs.data()); }
-
     uint256(uint64_t v) noexcept
     {
-        hn::Vec<D> limbs = hn::InsertLane(hn::Zero(D()), 0, v);
-        hn::Store(limbs, D(), this->limbs.data());
+        const hn::Vec<D> zero = hn::Zero(d);
+        hn::Vec<D> limbs = hn::InsertLane(zero, 0, v);
+        hn::Store(limbs, d, &this->limbs[0]);
+        if constexpr (hn::MaxLanes(d) < 4)
+            hn::Store(zero, d, &this->limbs[2]);
     }
 
-    operator bool() const { return !hn::AllBits0(D(), hn::Load(D(), limbs.data())); }
+    operator bool() const
+    {
+        if constexpr (hn::MaxLanes(d) < 4) {
+            return !hn::AllBits0(d, hn::Load(d, &limbs[0])) ||
+                   !hn::AllBits0(d, hn::Load(d, &limbs[2]));
+        } else {
+            return !hn::AllBits0(d, hn::Load(d, &limbs[0]));
+        }
+    }
 
     constexpr static uint256 ones(size_t n) noexcept
     {
@@ -73,9 +87,14 @@ struct uint256 {
 
     uint256 &operator|=(const uint256 &other) noexcept
     {
-        const hn::Vec<D> a = hn::Load(D(), limbs.data());
-        const hn::Vec<D> b = hn::Load(D(), other.limbs.data());
-        hn::Store(a | b, D(), limbs.data());
+        hn::Vec<D> a = hn::Load(d, &limbs[0]);
+        hn::Vec<D> b = hn::Load(d, &other.limbs[0]);
+        hn::Store(a | b, d, &limbs[0]);
+        if constexpr (hn::MaxLanes(d) < 4) {
+            a = hn::Load(d, &limbs[2]);
+            b = hn::Load(d, &other.limbs[2]);
+            hn::Store(a | b, d, &limbs[2]);
+        }
         return *this;
     }
 
@@ -88,9 +107,14 @@ struct uint256 {
 
     uint256 &operator&=(const uint256 &other) noexcept
     {
-        const hn::Vec<D> a = hn::Load(D(), limbs.data());
-        const hn::Vec<D> b = hn::Load(D(), other.limbs.data());
-        hn::Store(a & b, D(), limbs.data());
+        hn::Vec<D> a = hn::Load(d, &limbs[0]);
+        hn::Vec<D> b = hn::Load(d, &other.limbs[0]);
+        hn::Store(a & b, d, &limbs[0]);
+        if constexpr (hn::MaxLanes(d) < 4) {
+            a = hn::Load(d, &limbs[2]);
+            b = hn::Load(d, &other.limbs[2]);
+            hn::Store(a & b, d, &limbs[2]);
+        }
         return *this;
     }
 
@@ -103,29 +127,51 @@ struct uint256 {
 
     uint256 &operator^=(const uint256 &other) noexcept
     {
-        const hn::Vec<D> a = hn::Load(D(), limbs.data());
-        const hn::Vec<D> b = hn::Load(D(), other.limbs.data());
-        hn::Store(a ^ b, D(), limbs.data());
+        hn::Vec<D> a = hn::Load(d, &limbs[0]);
+        hn::Vec<D> b = hn::Load(d, &other.limbs[0]);
+        hn::Store(a ^ b, d, &limbs[0]);
+        if constexpr (hn::MaxLanes(d) < 4) {
+            a = hn::Load(d, &limbs[2]);
+            b = hn::Load(d, &other.limbs[2]);
+            hn::Store(a ^ b, d, &limbs[2]);
+        }
         return *this;
     }
 
     uint256 operator~() const noexcept
     {
-        return uint256(hn::Not(hn::Load(D(), limbs.data())));
+        uint256 result;
+        hn::Vec<D> v = hn::Load(d, &limbs[0]);
+        hn::Store(hn::Not(v), d, &result.limbs[0]);
+        if constexpr (hn::MaxLanes(d) < 4) {
+            v = hn::Load(d, &limbs[2]);
+            hn::Store(hn::Not(v), d, &result.limbs[2]);
+        }
+        return result;
+    }
+
+    static hn::Vec<D> shift_left1_vec(const hn::Vec<D> v) noexcept
+    {
+        const hn::Vec<D> shifted = hn::ShiftLeft<1>(v);
+        const hn::Vec<D> carries = hn::Slide1Up(d, hn::ShiftRight<63>(v));
+        return shifted | carries;
     }
 
     uint256 shift_left1() const noexcept
     {
-        const hn::Vec<D> a = hn::Load(D(), limbs.data());
-        const hn::Vec<D> shifted = hn::ShiftLeft<1>(a);
+        uint256 result;
 
-        // Carry bits between each 64-bit integer.
-        // TODO: Is there a better way to do this?
-        const hn::Vec<D> hi = hn::ShiftRight<63>(a);
-        alignas(32) uint64_t masks[] = {0, uint64_t(-1), uint64_t(-1), uint64_t(-1)};
-        const hn::Vec<D> carries = hn::Slide1Up(D(), hi) & hn::Load(D(), masks);
+        const uint64_t carry = limbs[1] >> 63; // only used for 2x128-bit case
+        hn::Vec<D> a = hn::Load(d, &limbs[0]);
+        hn::Store(shift_left1_vec(a), d, &result.limbs[0]);
 
-        return uint256(shifted | carries);
+        if constexpr (hn::MaxLanes(d) < 4) {
+            a = hn::Load(d, &limbs[2]);
+            hn::Store(shift_left1_vec(a), d, &result.limbs[2]);
+            result.limbs[2] = (result.limbs[2] & ~UINT64_C(1)) | carry;
+        }
+
+        return result;
     }
 
     /// Rotate left by 1 bit, wrapping around at the given bit width.
@@ -135,7 +181,6 @@ struct uint256 {
         const bool wrapped_bit = test_bit(n - 1);
         uint256 result = shift_left1();
 
-        // TODO: Is there a better way to do this?
         if (wrapped_bit) {
             result.set_bit(0);
             result.clear_bit(n);
@@ -144,18 +189,28 @@ struct uint256 {
         return result;
     }
 
+    static hn::Vec<D> shift_right1_vec(const hn::Vec<D> v) noexcept
+    {
+        const hn::Vec<D> shifted = hn::ShiftRight<1>(v);
+        const hn::Vec<D> carries = hn::Slide1Down(d, hn::ShiftLeft<63>(v));
+        return shifted | carries;
+    }
+
     uint256 shift_right1() const noexcept
     {
-        const hn::Vec<D> a = hn::Load(D(), limbs.data());
-        const hn::Vec<D> shifted = hn::ShiftRight<1>(a);
+        uint256 result;
 
-        // Carry bits between each 64-bit integer.
-        // TODO: Is there a better way to do this?
-        const hn::Vec<D> lo = hn::ShiftLeft<63>(a);
-        alignas(32) uint64_t masks[] = {uint64_t(-1), uint64_t(-1), uint64_t(-1), 0};
-        const hn::Vec<D> carries = hn::Slide1Down(D(), lo) & hn::Load(D(), masks);
+        const uint64_t carry = limbs[2] & 1; // only used for 2x128-bit case
+        hn::Vec<D> a = hn::Load(d, &limbs[0]);
+        hn::Store(shift_right1_vec(a), d, &result.limbs[0]);
 
-        return uint256(shifted | carries);
+        if constexpr (hn::MaxLanes(d) < 4) {
+            a = hn::Load(d, &limbs[2]);
+            hn::Store(shift_right1_vec(a), d, &result.limbs[2]);
+            result.limbs[1] = (result.limbs[1] & ~(UINT64_C(1) << 63)) | (carry << 63);
+        }
+
+        return result;
     }
 
     /// Rotate right by 1 bit, wrapping around at the given bit width.
@@ -165,7 +220,6 @@ struct uint256 {
         const bool wrapped_bit = test_bit(0);
         uint256 result = shift_right1();
 
-        // TODO: Is there a better way to do this?
         if (wrapped_bit)
             result.set_bit(n - 1);
 
