e1bad998df4e66d400dcffafc41131446692e5f3
Henrik Lehtonen <eigengrau@vm86.se>
2026-09-06 16:44:56 +0200

    2018/9: Optimize
    This started out as an effort to ditch the 256-bit vector requirement,
    but ended up being a complete rewrite. There's still a bunch of small
    stuff that I think could still be improved here, but meh. This is fine,
    especially considering the unholy mess that was the previous version.
    
          Year    Day    min t₀    min t₁     Δmin  Δmin%      avg t₀    avg t₁      Δavg  Δavg%
    --  ------  -----  --------  --------  -------  -------  --------  --------  --------  -------
          2018      9   13376.9   5962.53  -7414.4  -55.4%    14083.3    6195.3  -7888.01  -56.0%

--- 2018/9.cc
+++ 2018/9.cc
@@ -1,203 +1,79 @@
 #include <algorithm>
 #include <aoc/base.h>
+#include <aoc/macros.h>
 #include <aoc/string.h>
-#include <array>
 #include <cstddef>
 #include <cstdint>
-#include <cstdio>
-#include <hwy/highway.h>
 #include <memory>
 #include <string_view>
 #include <vector>
 
 namespace aoc_2018_9 {
 
-namespace hn = hwy::HWY_NAMESPACE;
-constexpr size_t block_capacity = 8;
-using D = hn::FixedTag<int32_t, block_capacity>;
-
-static const int32_t insert_shuffle_ctrl[8][8] = {
-    {0, 0, 1, 2, 3, 4, 5, 6}, {0, 0, 1, 2, 3, 4, 5, 6}, {0, 1, 0, 2, 3, 4, 5, 6},
-    {0, 1, 2, 0, 3, 4, 5, 6}, {0, 1, 2, 3, 0, 4, 5, 6}, {0, 1, 2, 3, 4, 0, 5, 6},
-    {0, 1, 2, 3, 4, 5, 0, 6}, {0, 1, 2, 3, 4, 5, 6, 0},
-};
-
-static const int32_t pop_shuffle_ctrl[8][8] = {
-    {1, 2, 3, 4, 5, 6, 7, 0}, {0, 2, 3, 4, 5, 6, 7, 0}, {0, 1, 3, 4, 5, 6, 7, 0},
-    {0, 1, 2, 4, 5, 6, 7, 0}, {0, 1, 2, 3, 5, 6, 7, 0}, {0, 1, 2, 3, 4, 6, 7, 0},
-    {0, 1, 2, 3, 4, 5, 7, 0}, {0, 1, 2, 3, 4, 5, 6, 0},
-};
-
-/// Instead of tracking marbles individually using a linked list, we track them
-/// in batches of max 8. This leads to better cache utilization due to having
-/// more contiguous values and lower space overhead (due to having one 'next'
-/// pointer per 8 elements instead of per each element).
-///
-/// The size 8 is specifically chosen to allow for quick insertion and deletion
-/// of values from an arbitrary index in the array with the vpermd instruction
-/// (_mm256_permutevar8x32_epi32) from AVX2.
-struct MarbleBlock {
-    std::array<int32_t, block_capacity> values;
-    uint32_t n;
-
-    // Since we allocate all blocks at once, they are contiguous. Shrink the
-    // structure a little by encoding the link to the next block as a byte
-    // offset relative to `this` stored as 4 bytes instead of a pointer (8
-    // bytes). This assumes ≤ 100,000,000 marbles in part 2, but my input only
-    // has around ~7,000,000.
-    int32_t next_offset;
-
-    void set_next(const MarbleBlock &other)
-    {
-        next_offset =
-            reinterpret_cast<intptr_t>(&other) - reinterpret_cast<intptr_t>(this);
-    }
-    MarbleBlock &next()
-    {
-        const auto p = reinterpret_cast<intptr_t>(this);
-        return *reinterpret_cast<MarbleBlock *>(p + next_offset);
-    }
-
-    /// Insert `value` into the given index in this block.
-    void insert(const size_t index, const int32_t value)
-    {
-        const hn::Vec<D> v = hn::LoadU(D(), values.data());
-        const auto shuf = hn::SetTableIndices(D(), insert_shuffle_ctrl[index]);
-        const hn::Vec<D> perm = hn::TableLookupLanes(v, shuf);
-        hn::StoreU(perm, D(), values.data());
-        values[index] = value;
-        n++;
-    }
-
-    /// Erase the marble at the given index in this block, returning it.
-    int32_t erase(const size_t index)
-    {
-        const int32_t removed = values[index];
-        const hn::Vec<D> v = hn::LoadU(D(), values.data());
-        const auto shuf = hn::SetTableIndices(D(), pop_shuffle_ctrl[index]);
-        const hn::Vec<D> perm = hn::TableLookupLanes(v, shuf);
-        hn::StoreU(perm, D(), values.data());
-        n--;
-        return removed;
-    }
-};
-
-static int64_t play(MarbleBlock *blocks, int n_players, int n_marbles)
+static uint64_t play(uint32_t *buffer, uint32_t n_players, uint32_t n_marbles)
 {
-    MarbleBlock *curr = blocks;
-    MarbleBlock *prev = curr;
-    MarbleBlock *next = curr;
-    curr->values = {0};
-    curr->n = 1;
-    curr->set_next(*curr);
+    std::vector<uint64_t> scores(n_players);
 
-    MarbleBlock *first_unused_block = blocks + 1;
-    MarbleBlock *last_unlinked_block = nullptr;
-    size_t block_offset = 0;
-    std::vector<int64_t> scores(n_players);
+    // TODO: Both `head` and `tail` grow monotonically (`head` 16/23 of the
+    // time in the core loop, `tail` 22/23 of the time). This could be a ring
+    // buffer to avoid having to allocate an over-sized array.
+    uint32_t *head = buffer;
+    uint32_t *tail = buffer;
 
-    auto insert_new_block = [&](MarbleBlock &a, MarbleBlock &b) -> MarbleBlock * {
-        MarbleBlock *block = last_unlinked_block;
-        last_unlinked_block = nullptr;
-        if (!block)
-            block = first_unused_block++;
+    *tail++ = 0;
 
-        block->n = 0;
-        block->set_next(b);
-        a.set_next(*block);
-        return block;
+    size_t i = 1;
+
+    auto append_marble = [&](size_t k) {
+        *tail++ = *head++;
+        *tail++ = k;
+    };
+    auto insert_marble = [&](size_t k, size_t insert_offset) {
+        *tail++ = k;
+        std::rotate(tail - 1 - insert_offset, tail - 1, tail);
+    };
+    auto remove_marble = [&](size_t k) {
+        scores[k % n_players] += k + tail[-8];
+        std::rotate(tail - 8, tail - 7, tail);
+        tail--;
     };
 
-    auto add_marble = [&](int32_t i) {
-        size_t new_marble_offset = block_offset + 2;
+    while (i < 23)
+        append_marble(i++);
 
-        if (new_marble_offset == curr->n + 1) {
-            // The most common case: we are at the end of a non-full block.
-            // Since we need to place the new marble two steps clockwise,
-            // extract the first marble of the next block to place it just
-            // before the new marble.
-            const int m = next->erase(0);
+    while (i + 23 <= n_marbles) {
+        remove_marble(i++);
+        insert_marble(i++, 5);
+        insert_marble(i++, 4);
+        insert_marble(i++, 3);
+        insert_marble(i++, 2);
+        insert_marble(i++, 1);
+        insert_marble(i++, 0);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+        append_marble(i++);
+    }
 
-            // If the next block is now empty, unhook it.
-            if (next->n == 0) [[unlikely]] {
-                curr->set_next(next->next());
-                if (curr->next_offset != 0) {
-                    // Mark this as the last freed block so that it can be
-                    // reused by the next call to insert_new_block().
-                    last_unlinked_block = next;
-                    next = &curr->next();
-                }
-            }
-
-            // If this block is full, insert a new block to place the new
-            // marble in.
-            if (curr->n >= block_capacity - 1) {
-                prev = curr;
-                curr = insert_new_block(*curr, *next);
-                next = &curr->next();
-            }
-
-            curr->values[curr->n++] = m;
-            curr->values[curr->n++] = i;
-            block_offset = curr->n - 1;
-            return;
-        } else if (curr->n < block_capacity) {
-            // If the new marble needs to be placed the middle of a non-full
-            // block, we can just insert it directly without any additional
-            // logic.
-            block_offset = new_marble_offset;
-            curr->insert(block_offset, i);
-            return;
-        }
-
-        // The current block is full, so a marble will have to be moved into
-        // the next block. Insert a new block inbetween if that one is full.
-        if (next->n == block_capacity)
-            next = insert_new_block(*curr, *next);
-
-        if (new_marble_offset == block_capacity) {
-            // Special case since insertions take two steps clockwise: if the
-            // current index is next to last and we are full, we need to insert
-            // the new marble as the first value into the next block.
-            block_offset = 0;
-            next->insert(0, i);
-            prev = curr;
-            curr = next;
-            next = &curr->next();
-        } else {
-            // The fallback and rarest case: the new marble needs to be placed
-            // into the middle of the current block. Kick out the last marble
-            // of this block into the next block to make space for it.
-            curr->n--;
-            next->insert(0, curr->values[curr->n]);
-            block_offset = new_marble_offset;
-            curr->insert(block_offset, i);
-        }
-    };
-
-    auto remove_marble = [&](int32_t i) {
-        auto new_marble_offset = static_cast<ssize_t>(block_offset) - 7;
-        if (new_marble_offset < 0) [[likely]] {
-            // The marble to be removed is in the previous block. (This is the
-            // common case; since we have 8 elements per block, the only way
-            // the marble can be in the same block is if we are currently on
-            // its last element, occurring only roughly 1/8 of the time.)
-            next = curr;
-            curr = prev;
-            prev = nullptr;
-            new_marble_offset += curr->n;
-        }
-
-        block_offset = new_marble_offset;
-        int removed_marble = curr->erase(block_offset);
-        scores[i % n_players] += i + removed_marble;
-    };
-
-    for (int i = 1; i <= n_marbles; i++) {
-        if (i % 23 != 0)
-            add_marble(i);
-        else
+    for (; i <= n_marbles; ++i) {
+        if (i % 23 == 0)
             remove_marble(i);
+        else if (i % 23 <= 6)
+            insert_marble(i, 6 - i % 23);
+        else
+            append_marble(i);
     }
 
     return std::ranges::max(scores);
@@ -205,11 +81,11 @@ static int64_t play(MarbleBlock *blocks, int n_players, int n_marbles)
 
 void run(std::string_view buf, aoc::Answer &answer)
 {
-    auto [n_players, n_marbles] = find_numbers_n<int, 2>(buf);
-    const auto num_blocks = (100 * n_marbles + 1 + 7) / 8;
-    auto blocks = std::make_unique_for_overwrite<MarbleBlock[]>(num_blocks);
-    answer.add(play(blocks.get(), n_players, n_marbles));
-    answer.add(play(blocks.get(), n_players, 100 * n_marbles));
+    auto [n_players, n_marbles] = find_numbers_n<uint32_t, 2>(buf);
+    ASSERT(UINT64_C(100) * n_marbles <= UINT32_MAX);
+    auto marbles = std::make_unique_for_overwrite<uint32_t[]>(200 * n_marbles);
+    answer.add(play(marbles.get(), n_players, n_marbles));
+    answer.add(play(marbles.get(), n_players, 100 * n_marbles));
 }
 AOC_REGISTER_SOLVER(2018, 9, run);
 
